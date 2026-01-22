0d0eed89063b55ed45b44a109cd2adc0567291cf
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-22 17:18:07 +0530

    Time: 1 ms (63.92%) | Memory: 19.9 MB (18.50%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -5,21 +5,15 @@ public:
         for (auto& num : nums)
             mp[num]++;
 
-        vector<pair<int, int>> frequencies;
-        for (auto& i : mp)
-            frequencies.push_back({i.first, i.second});
-
         vector<vector<int>> bucket(nums.size() + 1);
-        for (auto& freq : frequencies) {
-            bucket[freq.second].push_back(freq.first);
-        }
-        vector<int> ans(k);
-        k--;
+        for (auto& i : mp)
+            bucket[i.second].push_back(i.first);
+
+        vector<int> ans;
         for (int i = bucket.size() - 1; i >= 0; i--) {
             for (int j = bucket[i].size() - 1; j >= 0; j--) {
-                ans[k] = bucket[i][j];
-                k--;
-                if (k < 0)
+                ans.push_back(bucket[i][j]);
+                if (ans.size() == k)
                     return ans;
             }
         }
