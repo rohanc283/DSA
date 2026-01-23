7f0b9d14236895be97c5e22499147a809fed7bb1
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-23 18:59:36 +0530

    Time: 1 ms (92.25%) | Memory: 43.1 MB (42.79%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String customSortString(String order, String s) {
+    public String sol1(String order, String s) {
         int[] priorityList = new int[27];
         Arrays.fill(priorityList, Integer.MAX_VALUE);
 
@@ -20,5 +20,33 @@ class Solution {
             sb.append(c);
 
         return sb.toString();
+    }
+
+    public static String sol2(String order, String s) {
+        int[] freq = new int[27];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) {
+            int idx = order.charAt(i) - 'a';
+            while (freq[idx] > 0) {
+                sb.append(order.charAt(i));
+                freq[idx]--;
+            }
+        }
+
+        for (int i = 0; i < 27; i++) {
+            while (freq[i] > 0) {
+                char c = (char) ('a' + i);
+                sb.append(c);
+                freq[i]--;
+            }
+        }
+        return sb.toString();
+    }
+
+    public String customSortString(String order, String s) {
+        return sol2(order, s);
     }
 }
