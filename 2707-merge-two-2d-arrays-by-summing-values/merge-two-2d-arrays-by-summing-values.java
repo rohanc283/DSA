a4fece5bbb5dc260b8a2ade43d96b629780d94f7
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-24 13:20:04 +0530

    Time: 1 ms (88.27%) | Memory: 47 MB (34.91%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,33 +1,42 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        List<List<Integer>> idValueList = new ArrayList<>();
-        int i = 0, j = 0, n1 = nums1.length, n2 = nums2.length;
+        int n1 = nums1.length, n2 = nums2.length;
+        int[][] temp = new int[n1 + n2][2];
+
+        int i = 0, j = 0, k = 0;
+
         while (i < n1 && j < n2) {
             if (nums1[i][0] < nums2[j][0]) {
-                idValueList.add(new ArrayList<>(List.of(nums1[i][0], nums1[i][1])));
+                temp[k][0] = nums1[i][0];
+                temp[k][1] = nums1[i][1];
                 i++;
             } else if (nums1[i][0] > nums2[j][0]) {
-                idValueList.add(new ArrayList<>(List.of(nums2[j][0], nums2[j][1])));
+                temp[k][0] = nums2[j][0];
+                temp[k][1] = nums2[j][1];
                 j++;
             } else {
-                idValueList.add(new ArrayList<>(List.of(nums1[i][0], nums1[i][1] + nums2[j][1])));
+                temp[k][0] = nums1[i][0];
+                temp[k][1] = nums1[i][1] + nums2[j][1];
                 i++;
                 j++;
             }
+            k++;
         }
+
         while (i < n1) {
-            idValueList.add(new ArrayList<>(List.of(nums1[i][0], nums1[i][1])));
+            temp[k][0] = nums1[i][0];
+            temp[k][1] = nums1[i][1];
             i++;
+            k++;
         }
+
         while (j < n2) {
-            idValueList.add(new ArrayList<>(List.of(nums2[j][0], nums2[j][1])));
+            temp[k][0] = nums2[j][0];
+            temp[k][1] = nums2[j][1];
             j++;
+            k++;
         }
-        int[][] ans = new int[idValueList.size()][2];
-        for (i = 0; i < idValueList.size(); i++) {
-            ans[i][0] = idValueList.get(i).get(0);
-            ans[i][1] = idValueList.get(i).get(1);
-        }
-        return ans;
+
+        return Arrays.copyOf(temp, k);
     }
 }
