c6b03ed59bbcad84575602fcc277a78043d5c642
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-25 18:22:08 +0530

    Time: 1 ms (98.80%) | Memory: 43.2 MB (39.82%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.java
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean areSentencesSimilar(String sentence1, String sentence2) {
-        String[] sent1 = sentence1.split(" ");
-        String[] sent2 = sentence2.split(" ");
-        String[] shorter = sent1.length <= sent2.length ? sent1 : sent2;
-        String[] longer = sent2.length >= sent1.length ? sent2 : sent1;
+        String[] s1 = sentence1.split(" ");
+        String[] s2 = sentence2.split(" ");
+
+        String[] shorter = s1.length <= s2.length ? s1 : s2;
+        String[] longer = s1.length <= s2.length ? s2 : s1;
+
         int i = 0;
-        while (i < shorter.length && i < longer.length && shorter[i].equals(longer[i])) {
+
+        while (i < shorter.length && shorter[i].equals(longer[i])) {
             i++;
         }
-        if (i != 0 && i >= shorter.length)
-            return true;
+
         int j = shorter.length - 1;
         int k = longer.length - 1;
-        while (j >= 0 && shorter[j].equals(longer[k])) {
+
+        while (j >= i && shorter[j].equals(longer[k])) {
             j--;
             k--;
         }
-        if (i == 0 && j == 0)
-            return false;
-        if (i > j)
-            return true;
-        return false;
+        return j < i;
     }
 }
