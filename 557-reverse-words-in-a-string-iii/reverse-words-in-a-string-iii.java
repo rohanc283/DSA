fdfd3645b96627bb2d536e21895a0e0b94af3250
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-25 11:25:31 +0530

    Time: 9 ms (31.29%) | Memory: 46.6 MB (79.24%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,20 +1,21 @@
 class Solution {
+
+    private void reverse(StringBuilder sb, int l, int r) {
+        while (l < r) {
+            char temp = sb.charAt(l);
+            sb.setCharAt(l++, sb.charAt(r));
+            sb.setCharAt(r--, temp);
+        }
+    }
+
     public String reverseWords(String str) {
         StringBuilder sb = new StringBuilder(str);
-        int s = 0, n = sb.length();
-        for (int i = 0; i < n; i++) {
-            if (sb.charAt(i) == ' ') {
-                s = i + 1;
-                continue;
-            }
-            if (i == n - 1 || sb.charAt(i + 1) == ' ') {
-                int x = s, y = i;
-                while (x < y) {
-                    char c = sb.charAt(x);
-                    sb.setCharAt(x++, sb.charAt(y));
-                    sb.setCharAt(y--, c);
-                }
-                s = i + 1;
+        int start = 0;
+
+        for (int i = 0; i <= sb.length(); i++) {
+            if (i == sb.length() || sb.charAt(i) == ' ') {
+                reverse(sb, start, i - 1);
+                start = i + 1;
             }
         }
         return sb.toString();
