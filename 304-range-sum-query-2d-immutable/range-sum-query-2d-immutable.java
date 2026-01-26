a74ab286e20fbfe55150ea53733e80dc0a03ce5c
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-26 18:08:58 +0530

    Time: 101 ms (84.23%) | Memory: 142.2 MB (39.50%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -10,12 +10,6 @@ class NumMatrix {
                         - prefix[i - 1][j - 1];
             }
         }
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                System.out.print(prefix[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
