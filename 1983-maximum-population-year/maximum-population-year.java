8703442f4c8791844c4238c2e20beb57e0eda010
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-27 20:42:18 +0530

    Time: 3 ms (20.43%) | Memory: 43.9 MB (17.38%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,28 +1,24 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
         Map<Integer, Integer> events = new TreeMap<>();
-        int n = logs.length;
-        for (int i = 0; i < n; i++) {
-            int s = logs[i][0];
-            int e = logs[i][1];
-            events.put(s, events.getOrDefault(s, 0) + 1);
-            events.put(e, events.getOrDefault(e, 0) - 1);
-        }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(events.entrySet());
-        list.sort((e1, e2) -> {
-            int keyCmp = Integer.compare(e1.getKey(), e2.getKey());
-            return keyCmp;
-        });
 
-        int maxPop = Integer.MIN_VALUE;
-        int currPop = 0, maxPopYear = 0;
-        for (Map.Entry<Integer, Integer> entry : list) {
+        for (int[] log : logs) {
+            events.put(log[0], events.getOrDefault(log[0], 0) + 1);
+            events.put(log[1], events.getOrDefault(log[1], 0) - 1);
+        }
+
+        int currPop = 0;
+        int maxPop = 0;
+        int maxYear = 0;
+
+        for (Map.Entry<Integer, Integer> entry : events.entrySet()) {
             currPop += entry.getValue();
             if (currPop > maxPop) {
                 maxPop = currPop;
-                maxPopYear = entry.getKey();
+                maxYear = entry.getKey();
             }
         }
-        return maxPopYear;
+
+        return maxYear;
     }
 }
