73e658c67bb37824966004f613ecac443d7d56e5
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-27 14:37:49 +0530

    Time: 63 ms (51.03%) | Memory: 121.3 MB (14.07%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -27,7 +27,39 @@ class Solution {
         return res;
     }
 
+    public int diffArrayTech(int[] nums, int k, int numOperations) {
+        int maxEl = Integer.MIN_VALUE;
+        int n = nums.length;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            maxEl = Math.max(maxEl, nums[i]);
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int[] diff = new int[maxEl + 1];
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int left = Math.max(0, num - k);
+            int right = Math.min(maxEl, num + k);
+            diff[left]++;
+            if (right + 1 <= maxEl) {
+                diff[right + 1]--;
+            }
+        }
+
+        int res = 0;
+        for (int target = 0; target <= maxEl; target++) {
+            diff[target] += (target > 0 ? diff[target - 1] : 0);
+            int targetCount = freq.getOrDefault(target, 0);
+            int needConversion = diff[target] - targetCount;
+            int maxPosFreq = targetCount + Math.min(numOperations, needConversion);
+            res = Math.max(res, maxPosFreq);
+        }
+        return res;
+    }
+
     public int maxFrequency(int[] nums, int k, int numOperations) {
-        return prefixSumTech(nums, k, numOperations);
+        // return prefixSumTech(nums, k, numOperations);
+        return diffArrayTech(nums, k, numOperations);
     }
 }
