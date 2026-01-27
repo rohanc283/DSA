dfbedef700cf70600c459b410e1f02fe0375b806
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-27 13:35:55 +0530

    Time: 7 ms (97.36%) | Memory: 94.9 MB (65.69%) - LeetSync

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        int maxEl = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            maxEl = Math.max(maxEl, nums[i]);
+        }
+        int[] freq = new int[maxEl + 1];
+        for (int i = 0; i < n; i++)
+            freq[nums[i]]++;
+
+        for (int i = 1; i <= maxEl; i++)
+            freq[i] += freq[i - 1];
+
+        int res = 0;
+        for (int target = 0; target <= maxEl; target++) {
+            if (freq[target] == 0)
+                continue;
+            int leftNum = Math.max(0, target - k);
+            int rightNum = Math.min(maxEl, target + k);
+            int totalCount = freq[rightNum] - (leftNum > 0 ? freq[leftNum - 1] : 0);
+            int targetCount = freq[target] - (target > 0 ? freq[target - 1] : 0);
+            int needConversion = totalCount - targetCount;
+            int maxPosFreq = targetCount + Math.min(numOperations, needConversion);
+            res = Math.max(res, maxPosFreq);
+        }
+        return res;
+    }
+}
