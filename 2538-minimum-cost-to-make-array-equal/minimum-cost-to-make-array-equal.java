2f75136dee385228a496a40d091df25736c05986
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-28 12:08:48 +0530

    Time: 23 ms (9.50%) | Memory: 67.2 MB (9.50%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
@@ -7,7 +7,7 @@ class Solution {
         return total;
     }
 
-    public long minCost(int[] nums, int[] cost) {
+    public long ternarySearch(int[] nums, int[] cost) {
         long l = Long.MAX_VALUE, r = Long.MIN_VALUE;
         for (int num : nums) {
             l = Math.min(l, num);
@@ -27,5 +27,37 @@ class Solution {
                 l = mid + 1;
         }
         return ans;
+    }
+
+    public long prefix(int[] nums, int[] cost) {
+        int n = nums.length;
+
+        Integer[][] numsWithCost = new Integer[n][2];
+        long totalCost = 0;
+
+        for (int i = 0; i < n; i++) {
+            numsWithCost[i][0] = nums[i];
+            numsWithCost[i][1] = cost[i];
+            totalCost += cost[i];
+        }
+
+        Arrays.sort(numsWithCost, (a, b) -> Integer.compare(a[0], b[0]));
+
+        long prefix = 0;
+        long median = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefix += numsWithCost[i][1];
+            if (prefix * 2 >= totalCost) {
+                median = numsWithCost[i][0];
+                break;
+            }
+        }
+
+        return findCost(nums, cost, median);
+    }
+
+    public long minCost(int[] nums, int[] cost) {
+        return prefix(nums, cost);
     }
 }
