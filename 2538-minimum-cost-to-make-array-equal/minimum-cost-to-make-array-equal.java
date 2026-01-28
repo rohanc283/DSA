6b400a13e8d9f255bc4c4c4475405d01a1cd33c1
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-28 11:05:47 +0530

    Time: 12 ms (77.09%) | Memory: 61 MB (38.55%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
@@ -1,44 +1,31 @@
 class Solution {
-    public long findCost(int[] nums, int n, int[] cost, long k) {
-        long currCost = 0;
-        for (int i = 0; i < n; i++) {
-            long diff = Math.abs(k - nums[i]);
-            currCost += (diff * cost[i]);
+    public long findCost(int[] nums, int[] cost, long x) {
+        long total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += Math.abs(nums[i] - x) * cost[i];
         }
-        return currCost;
-    }
-
-    public boolean alreadyEqual(int[] nums, int n) {
-        for (int i = 1; i < n; i++) {
-            if (nums[i] != nums[0])
-                return false;
-        }
-        return true;
+        return total;
     }
 
     public long minCost(int[] nums, int[] cost) {
-        int n = nums.length;
-        if (alreadyEqual(nums, n))
-            return 0;
-        long l = Integer.MAX_VALUE, r = Integer.MIN_VALUE;
+        long l = Long.MAX_VALUE, r = Long.MIN_VALUE;
         for (int num : nums) {
             l = Math.min(l, num);
             r = Math.max(r, num);
         }
 
-        long[] ans = new long[1];
-        ans[0] = Long.MAX_VALUE;
+        long ans = Long.MAX_VALUE;
         while (l <= r) {
             long mid = l + (r - l) / 2;
-            long midCost1 = findCost(nums, n, cost, mid);
-            long midCost2 = findCost(nums, n, cost, mid + 1);
-            ans[0] = Math.min(ans[0], Math.min(midCost1, midCost2));
-            if (midCost1 < midCost2) {
+            long c1 = findCost(nums, cost, mid);
+            long c2 = findCost(nums, cost, mid + 1);
+
+            ans = Math.min(ans, Math.min(c1, c2));
+            if (c1 < c2)
                 r = mid - 1;
-            } else {
+            else
                 l = mid + 1;
-            }
         }
-        return ans[0];
+        return ans;
     }
 }
