a2c8633a7ebabd58edb57033da967bf187206a33
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-28 21:20:05 +0530

    Time: 1 ms (59.33%) | Memory: 47.1 MB (94.84%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
+    public void sol1(int[][] matrix) {
         List<List<Integer>> zeros = new ArrayList<>();
         int m = matrix.length, n = matrix[0].length;
         for (int i = 0; i < m; i++) {
@@ -20,5 +20,59 @@ class Solution {
                 matrix[j][c] = 0;
             }
         }
+    }
+
+    public void sol2(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean firstRowZero = false, firstColZero = false;
+        // First row impact check
+        for (int c = 0; c < n; c++) {
+            if (matrix[0][c] == 0) {
+                firstRowZero = true;
+            }
+        }
+
+        // First col impact check
+        for (int r = 0; r < m; r++) {
+            if (matrix[r][0] == 0) {
+                firstColZero = true;
+            }
+        }
+
+        // If any ele is 0 then mark that row[0] and col[0] as 0
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Populate 0 with help of row[0] and col[0]
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Populate First row back
+        for (int c = 0; c < n; c++) {
+            if (firstRowZero)
+                matrix[0][c] = 0;
+        }
+
+        // Populate First col back
+        for (int r = 0; r < m; r++) {
+            if (firstColZero) {
+                matrix[r][0] = 0;
+            }
+        }
+    }
+
+    public void setZeroes(int[][] matrix) {
+        sol2(matrix);
     }
 }
