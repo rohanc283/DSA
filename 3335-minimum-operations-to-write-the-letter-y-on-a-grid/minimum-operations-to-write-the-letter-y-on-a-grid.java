7ac2b4298dca2c94887b2e56c4ca39aaff9d24ef
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-29 12:30:08 +0530

    Time: 2 ms (91.59%) | Memory: 47 MB (76.14%) - LeetSync

--- 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.java
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.java
@@ -18,18 +18,56 @@ class Solution {
         return cost;
     }
 
-    public int minimumOperationsToWriteY(int[][] grid) {
-        int ans = Integer.MAX_VALUE;
-        int[] values = { 0, 1, 2 };
+    public int freqSol(int[][] grid) {
+        int n = grid.length, centre = n / 2;
 
-        for (int yVal : values) {
-            for (int oVal : values) {
+        int[] freqY = new int[3];
+        int[] freqNonY = new int[3];
+        int totalY = 0, totalNonY = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isY = (i == j && i < centre) ||
+                        (i + j == n - 1 && i < centre) ||
+                        (i >= centre && j == centre);
+
+                if (isY) {
+                    freqY[grid[i][j]]++;
+                    totalY++;
+                } else {
+                    freqNonY[grid[i][j]]++;
+                    totalNonY++;
+                }
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for (int yVal = 0; yVal <= 2; yVal++) {
+            for (int oVal = 0; oVal <= 2; oVal++) {
                 if (yVal == oVal)
                     continue;
-                ans = Math.min(ans, findCost(grid, yVal, oVal));
+                int cost = (totalY - freqY[yVal]) + (totalNonY - freqNonY[oVal]);
+                ans = Math.min(ans, cost);
             }
         }
 
         return ans;
+    }
+
+    public int minimumOperationsToWriteY(int[][] grid) {
+        // int ans = Integer.MAX_VALUE;
+        // int[] values = { 0, 1, 2 };
+
+        // for (int yVal : values) {
+        //     for (int oVal : values) {
+        //         if (yVal == oVal)
+        //             continue;
+        //         ans = Math.min(ans, findCost(grid, yVal, oVal));
+        //     }
+        // }
+
+        // return ans;
+        return freqSol(grid);
     }
 }
