4131231619bc076c0e43f6cd942f216db4ab4087
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-29 18:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (98.07%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,51 +1,42 @@
 class Solution {
-    public boolean canCellLiveMore(int[][] board, int m, int n, int i, int j) {
-        int liveCellCount = 0;
-        int[] iPos = { -1, -1, -1, 0, 0, 1, 1, 1 };
-        int[] jPos = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
-        for (int x = 0; x < 8; x++) {
-            int r = i + iPos[x], c = j + jPos[x];
-            if (r < 0 || r >= m || c < 0 || c >= n)
-                continue;
+    private static final int[] DIR = { -1, 0, 1 };
 
-            if (board[r][c] == 1)
-                liveCellCount++;
+    private int countLiveNeighbors(int[][] board, int m, int n, int i, int j) {
+        int count = 0;
+
+        for (int di : DIR) {
+            for (int dj : DIR) {
+                if (di == 0 && dj == 0)
+                    continue;
+
+                int r = i + di, c = j + dj;
+                if (r >= 0 && r < m && c >= 0 && c < n && board[r][c] == 1) {
+                    count++;
+                }
+            }
         }
-        return liveCellCount == 2 || liveCellCount == 3;
-    }
-
-    public boolean canDeadCellLive(int[][] board, int m, int n, int i, int j) {
-        int liveCellCount = 0;
-        int[] iPos = { -1, -1, -1, 0, 0, 1, 1, 1 };
-        int[] jPos = { -1, 0, 1, -1, 1, -1, 0, 1 };
-
-        for (int x = 0; x < 8; x++) {
-            int r = i + iPos[x], c = j + jPos[x];
-            if (r < 0 || r >= m || c < 0 || c >= n)
-                continue;
-            if (board[r][c] == 1)
-                liveCellCount++;
-        }
-        return liveCellCount == 3;
+        return count;
     }
 
     public void gameOfLife(int[][] board) {
         int m = board.length, n = board[0].length;
-        int[][] res = new int[m][n];
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
+        int[][] next = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = countLiveNeighbors(board, m, n, i, j);
+
                 if (board[i][j] == 1) {
-                    res[i][j] = canCellLiveMore(board, m, n, i, j) ? 1 : 0;
+                    next[i][j] = (liveNeighbors == 2 || liveNeighbors == 3) ? 1 : 0;
                 } else {
-                    res[i][j] = canDeadCellLive(board, m, n, i, j) ? 1 : 0;
+                    next[i][j] = (liveNeighbors == 3) ? 1 : 0;
                 }
             }
         }
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                board[i][j] = res[i][j];
-            }
+
+        for (int i = 0; i < m; i++) {
+            System.arraycopy(next[i], 0, board[i], 0, n);
         }
     }
 }
