e30cecee6e1a01d8d8c929c2ad3e8c93fb9fecd8
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-29 18:55:47 +0530

    Time: 15 ms (36.36%) | Memory: 48.6 MB (9.83%) - LeetSync

--- 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.java
+++ 1094-matrix-cells-in-distance-order/matrix-cells-in-distance-order.java
@@ -1,21 +1,25 @@
 class Solution {
     public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+
         int[][] res = new int[rows * cols][2];
-        Map<Integer, List<List<Integer>>> sortedDisMap = new TreeMap<>();
+        Map<Integer, List<int[]>> distMap = new TreeMap<>();
+
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 int dist = Math.abs(i - rCenter) + Math.abs(j - cCenter);
-                sortedDisMap.computeIfAbsent(dist, k -> new ArrayList<>()).add(new ArrayList<>(List.of(i, j)));
+                distMap
+                        .computeIfAbsent(dist, d -> new ArrayList<>())
+                        .add(new int[] { i, j });
             }
         }
-        int k = 0;
-        for (Map.Entry<Integer, List<List<Integer>>> entry : sortedDisMap.entrySet()) {
-            for (List<Integer> list : entry.getValue()) {
-                res[k][0] = list.get(0);
-                res[k][1] = list.get(1);
-                k++;
+
+        int idx = 0;
+        for (List<int[]> cells : distMap.values()) {
+            for (int[] cell : cells) {
+                res[idx++] = cell;
             }
         }
+
         return res;
     }
 }
