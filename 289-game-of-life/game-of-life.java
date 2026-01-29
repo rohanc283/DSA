6f13ffa5fb49c5ee22c7c68a651165ddbcabb6a2
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-29 18:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (87.63%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -19,7 +19,7 @@ class Solution {
         return count;
     }
 
-    public void gameOfLife(int[][] board) {
+    public void withSpace(int[][] board) {
         int m = board.length, n = board[0].length;
         int[][] next = new int[m][n];
 
@@ -38,5 +38,50 @@ class Solution {
         for (int i = 0; i < m; i++) {
             System.arraycopy(next[i], 0, board[i], 0, n);
         }
+    }
+
+    private int countLiveNeighbors2(int[][] board, int m, int n, int i, int j) {
+        int count = 0;
+
+        for (int di : DIR) {
+            for (int dj : DIR) {
+                if (di == 0 && dj == 0)
+                    continue;
+
+                int r = i + di, c = j + dj;
+                if (r >= 0 && r < m && c >= 0 && c < n) {
+                    if (board[r][c] == 1 || board[r][c] == 2)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void inPlace(int[][] board) {
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int liveNeighbors = countLiveNeighbors2(board, m, n, i, j);
+                if (board[i][j] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3)
+                        board[i][j] = 2;
+                } else {
+                    if (liveNeighbors == 3)
+                        board[i][j] = 3;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] %= 2;
+            }
+        }
+    }
+
+    public void gameOfLife(int[][] board) {
+        inPlace(board);
     }
 }
