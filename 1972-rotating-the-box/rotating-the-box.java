35c6a8ace4a3d38fe969453cb2eea36a23c535b2
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-29 15:58:57 +0530

    Time: 8 ms (56.75%) | Memory: 125 MB (21.73%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -2,8 +2,12 @@ class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
         int m = boxGrid.length, n = boxGrid[0].length;
         char[][] res = new char[n][m];
+        for (char[] row : res) {
+            Arrays.fill(row, '.');
+        }
         int[] freeSpace = new int[m];
         Arrays.fill(freeSpace, n - 1);
+
         for (int c = n - 1; c >= 0; c--) {
             for (int r = m - 1; r >= 0; r--) {
                 int resColIdx = m - r - 1;
@@ -14,13 +18,6 @@ class Solution {
                     int resRowIdx = freeSpace[resColIdx];
                     res[resRowIdx][resColIdx] = boxGrid[r][c];
                     freeSpace[resColIdx]--;
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (res[i][j] == '\0') {
-                    res[i][j] = '.';
                 }
             }
         }
