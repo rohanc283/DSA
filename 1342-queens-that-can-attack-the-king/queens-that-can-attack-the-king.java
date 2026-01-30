3fdeb270e133e6077c0a36b5dc6825fcb3927dac
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-30 10:02:58 +0530

    Time: 6 ms (6.10%) | Memory: 50.1 MB (11.59%) - LeetSync

--- 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.java
+++ 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+    public List<List<Integer>> withSpace(int[][] queens, int[] king) {
         int[][] board = new int[8][8];
         board[king[0]][king[1]] = 1;
         for (int i = 0; i < queens.length; i++) {
@@ -15,6 +15,33 @@ class Solution {
                 int c = king[1] + j;
                 while (r >= 0 && r < 8 && c >= 0 && c < 8) {
                     if (board[r][c] == 2) {
+                        res.add(new ArrayList<>(List.of(r, c)));
+                        break;
+                    }
+                    r += i;
+                    c += j;
+                }
+            }
+        }
+        return res;
+    }
+
+    public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
+        Set<String> queensPos = new HashSet<>();
+        for (int i = 0; i < queens.length; i++) {
+            queensPos.add(queens[i][0] + "," + queens[i][1]);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        int[] dir = { -1, 0, 1 };
+        for (int i : dir) {
+            for (int j : dir) {
+                if (i == 0 && j == 0)
+                    continue;
+                int r = king[0] + i;
+                int c = king[1] + j;
+                while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                    if (queensPos.contains(r + "," + c)) {
                         res.add(new ArrayList<>(List.of(r, c)));
                         break;
                     }
