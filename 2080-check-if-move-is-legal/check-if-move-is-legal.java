bb9f3c7c8a21ee6593d83a6d858af63703736b0a
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-30 17:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (11.32%) - LeetSync

--- 2080-check-if-move-is-legal/check-if-move-is-legal.java
+++ 2080-check-if-move-is-legal/check-if-move-is-legal.java
@@ -1,44 +1,31 @@
 class Solution {
     public boolean checkMove(char[][] board, int rMove, int cMove, char color) {
+
         int[] dir = { -1, 0, 1 };
-        for (int x : dir) {
-            for (int y : dir) {
-                if (x == 0 && y == 0)
+        char opp = (color == 'B') ? 'W' : 'B';
+
+        for (int dx : dir) {
+            for (int dy : dir) {
+                if (dx == 0 && dy == 0)
                     continue;
-                int r = rMove + x, c = cMove + y;
-                int wCount = 0, bCount = 0;
-                if (color == 'B') {
-                    while (r >= 0 && r < 8 && c >= 0 && c < 8) {
-                        if (board[r][c] == '.')
-                            break;
-                        else if (board[r][c] == 'W')
-                            wCount++;
-                        else {
-                            if (wCount > 0)
-                                return true;
-                            else {
-                                break;
-                            }
-                        }
-                        r += x;
-                        c += y;
+
+                int r = rMove + dx, c = cMove + dy;
+                boolean seenOpp = false;
+
+                while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                    if (board[r][c] == '.')
+                        break;
+
+                    if (board[r][c] == opp) {
+                        seenOpp = true;
+                    } else {
+                        if (seenOpp)
+                            return true;
+                        break;
                     }
-                } else {
-                    while (r >= 0 && r < 8 && c >= 0 && c < 8) {
-                        if (board[r][c] == '.')
-                            break;
-                        else if (board[r][c] == 'B')
-                            bCount++;
-                        else {
-                            if (bCount > 0)
-                                return true;
-                            else {
-                                break;
-                            }
-                        }
-                        r += x;
-                        c += y;
-                    }
+
+                    r += dx;
+                    c += dy;
                 }
             }
         }
