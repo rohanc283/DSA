68f02f7d333a5a8e9438933f1a443fabf44f5c14
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-31 16:24:01 +0530

    Time: 0 ms (100.00%) | Memory: 82.8 MB (18.69%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.java
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean prime(int num) {
         if (num == 0 || num == 1)
             return false;
-        for (int i = 2; i < num / 2; i++) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0)
                 return false;
         }
@@ -12,11 +12,13 @@ class Solution {
     public int diagonalPrime(int[][] nums) {
         int n = nums.length;
         int res = 0;
+
         for (int i = 0, j = 0; i < n && j < n; i++, j++) {
             if (nums[i][j] > res && prime(nums[i][j])) {
                 res = nums[i][j];
             }
         }
+
         for (int i = 0, j = n - 1; i < n && j >= 0; i++, j--) {
             if (nums[i][j] > res && prime(nums[i][j])) {
                 res = nums[i][j];
