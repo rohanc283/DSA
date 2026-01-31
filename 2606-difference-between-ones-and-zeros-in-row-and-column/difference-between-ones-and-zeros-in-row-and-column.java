8cd117020fbd6dc54a5747b00b6e8c41f11326fe
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-31 16:03:59 +0530

    Time: 7 ms (85.03%) | Memory: 137.9 MB (93.91%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.java
@@ -1,6 +1,5 @@
 class Solution {
-    public int[][] onesMinusZeros(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+    public int[][] withSpace(int[][] grid, int m, int n) {
         int[][] rowCounts = new int[m][2];
         int[][] colCounts = new int[n][2];
 
@@ -30,5 +29,52 @@ class Solution {
             }
         }
         return grid;
+    }
+
+    public int[][] inPlace(int[][] grid, int m, int n) {
+        int firstColOneCount = 0;
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1)
+                firstColOneCount++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int oneCount = 0;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1)
+                    oneCount++;
+            }
+            grid[i][0] = oneCount;
+        }
+
+        for (int j = 1; j < n; j++) {
+            int oneCount = 0;
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 1)
+                    oneCount++;
+            }
+            grid[0][j] = oneCount;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i][0] + grid[0][j] - (n - grid[i][0]) - (m - grid[0][j]);
+            }
+        }
+
+        for (int j = 1; j < n; j++) {
+            grid[0][j] = grid[0][0] + grid[0][j] - (n - grid[0][0]) - (m - grid[0][j]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            grid[i][0] = grid[i][0] + firstColOneCount - (n - grid[i][0]) - (m - firstColOneCount);
+        }
+        return grid;
+    }
+
+    public int[][] onesMinusZeros(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        return inPlace(grid, m, n);
     }
 }
