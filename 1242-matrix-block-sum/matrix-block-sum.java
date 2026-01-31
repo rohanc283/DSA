2e60496a88661910ffc1fe76c30541800e7988ac
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-31 17:16:24 +0530

    Time: 7 ms (55.80%) | Memory: 46.8 MB (56.90%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[][] matrixBlockSum(int[][] mat, int k) {
+    public int[][] solUsingRowPrefix(int[][] mat, int k) {
         int m = mat.length, n = mat[0].length;
         int[][] prefix = new int[m][n];
 
@@ -23,7 +23,45 @@ class Solution {
                 mat[i][j] = sum;
             }
         }
-
         return mat;
+    }
+
+    public int[][] solUsingMatrixPrefix(int[][] mat, int k) {
+        int m = mat.length, n = mat[0].length;
+
+        // Build 2D prefix sum
+        int[][] prefix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int top = i > 0 ? prefix[i - 1][j] : 0;
+                int left = j > 0 ? prefix[i][j - 1] : 0;
+                int diag = (i > 0 && j > 0) ? prefix[i - 1][j - 1] : 0;
+                prefix[i][j] = mat[i][j] + top + left - diag;
+            }
+        }
+
+        // Compute block sums
+        int[][] res = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int r1 = Math.max(0, i - k);
+                int c1 = Math.max(0, j - k);
+                int r2 = Math.min(m - 1, i + k);
+                int c2 = Math.min(n - 1, j + k);
+
+                int total = prefix[r2][c2];
+                int top = r1 > 0 ? prefix[r1 - 1][c2] : 0;
+                int left = c1 > 0 ? prefix[r2][c1 - 1] : 0;
+                int diag = (r1 > 0 && c1 > 0) ? prefix[r1 - 1][c1 - 1] : 0;
+
+                res[i][j] = total - top - left + diag;
+            }
+        }
+
+        return res;
+    }
+
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        return solUsingMatrixPrefix(mat, k);
     }
 }
