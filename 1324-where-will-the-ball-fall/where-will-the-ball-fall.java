1ae9f63742022628c7dc40cc5cf882ef0e61449a
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-01-31 10:27:36 +0530

    Time: 3 ms (65.97%) | Memory: 47.4 MB (25.00%) - LeetSync

--- 1324-where-will-the-ball-fall/where-will-the-ball-fall.java
+++ 1324-where-will-the-ball-fall/where-will-the-ball-fall.java
@@ -2,28 +2,25 @@ class Solution {
     public int[] findBall(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         int[] res = new int[n];
+
         for (int c = 0; c < n; c++) {
-            int j = c, i = 0;
-            while (i < m && j >= 0 && j < n) {
-                if (j == n - 1 && grid[i][j] == 1) {
-                    j = -1;
+            int col = c;
+
+            for (int row = 0; row < m; row++) {
+                int dir = grid[row][col];
+                int nextCol = col + dir;
+
+                if (nextCol < 0 || nextCol >= n || grid[row][nextCol] != dir) {
+                    col = -1;
                     break;
-                } else if (j == 0 && grid[i][j] == -1) {
-                    j = -1;
-                    break;
-                } else if (grid[i][j] == 1 && j + 1 < n && grid[i][j + 1] == -1) {
-                    j = -1;
-                    break;
-                } else if (grid[i][j] == -1 && j - 1 >= 0 && grid[i][j - 1] == 1) {
-                    j = -1;
-                    break;
-                } else {
-                    j += grid[i][j];
-                    i++;
                 }
+
+                col = nextCol;
             }
-            res[c] = j;
+
+            res[c] = col;
         }
+
         return res;
     }
 }
