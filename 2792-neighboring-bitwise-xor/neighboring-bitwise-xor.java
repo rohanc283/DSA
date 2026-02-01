f723efe618119b08251ae4e77b871354e5cc090f
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-01 16:10:44 +0530

    Time: 2 ms (100.00%) | Memory: 199.5 MB (9.23%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -2,10 +2,12 @@ class Solution {
     public boolean checkXor(int[] derived, int start) {
         int n = derived.length;
         int prev = start;
+
         for (int i = 0; i < n - 1; i++) {
-            int curr = (derived[i] ^ prev);
+            int curr = derived[i] ^ prev;
             prev = curr;
         }
+
         return (prev ^ start) == derived[n - 1];
     }
 
