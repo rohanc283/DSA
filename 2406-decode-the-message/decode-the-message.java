072132e2d0ba463cb8f8933a21b2ede60aa1951e
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-02 12:13:40 +0530

    Time: 2 ms (99.82%) | Memory: 43.7 MB (89.46%) - LeetSync

--- 2406-decode-the-message/decode-the-message.java
+++ 2406-decode-the-message/decode-the-message.java
@@ -1,21 +1,21 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        char[] mapping = new char[26];
-        boolean[] marked = new boolean[26];
-        int i = 0;
+        char[] map = new char[26];
+        boolean[] seen = new boolean[26];
+        int idx = 0;
+
         for (char c : key.toCharArray()) {
             if (c == ' ')
                 continue;
-
-            if (!marked[c - 'a']) {
-                mapping[c - 'a'] = (char) (i + 'a');
-                i++;
+            if (!seen[c - 'a']) {
+                map[c - 'a'] = (char) (idx++ + 'a');
+                seen[c - 'a'] = true;
             }
-            marked[c - 'a'] = true;
         }
+
         StringBuilder sb = new StringBuilder();
         for (char c : message.toCharArray()) {
-            sb.append(c == ' ' ? c : (char) mapping[c - 'a']);
+            sb.append(c == ' ' ? ' ' : map[c - 'a']);
         }
         return sb.toString();
     }
