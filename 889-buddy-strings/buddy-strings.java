a1b5f70775da16699339057bd943861ce674d2c6
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-03 11:43:12 +0530

    Time: 1 ms (99.60%) | Memory: 44.1 MB (26.81%) - LeetSync

---
+++ 889-buddy-strings/buddy-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
+
+        if (s.equals(goal)) {
+            int[] freq = new int[26];
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
+                if (freq[c - 'a'] > 1)
+                    return true;
+            }
+        }
+
+        int first = -1, second = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != goal.charAt(i)) {
+                if (first == -1)
+                    first = i;
+                else if (second == -1)
+                    second = i;
+                else
+                    return false;
+            }
+        }
+
+        return first != -1 && second != -1 && s.charAt(first) == goal.charAt(second)
+                && s.charAt(second) == goal.charAt(first);
+    }
+}
