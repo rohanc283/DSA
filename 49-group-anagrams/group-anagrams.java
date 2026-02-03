8d45df6e174af72bd9678d9f2c89aa91e3d0e727
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-03 14:31:27 +0530

    Time: 8 ms (43.11%) | Memory: 50.1 MB (25.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,58 +1,15 @@
 class Solution {
-
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
-        }
-
-        for (int f : freq)
-            if (f != 0)
-                return false;
-
-        return true;
-    }
-
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
 
-        map.computeIfAbsent(strs[0], k -> new ArrayList<>());
+        for (String word : strs) {
+            char[] arr = word.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
 
-        for (int i = 1; i < strs.length; i++) {
-            boolean foundAnagram = false;
-
-            for (String key : map.keySet()) {
-                if (isAnagram(strs[i], key)) {
-                    map.get(key).add(strs[i]);
-                    foundAnagram = true;
-                    break;
-                }
-            }
-
-            if (!foundAnagram) {
-                map.computeIfAbsent(strs[i], k -> new ArrayList<>());
-            }
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
         }
 
-        List<List<String>> res = new ArrayList<>();
-
-        for (String key : map.keySet()) {
-            List<String> curr = new ArrayList<>();
-            curr.add(key);
-
-            List<String> values = map.getOrDefault(key, new ArrayList<>());
-
-            for (String val : values) {
-                curr.add(val);
-            }
-
-            res.add(curr);
-        }
-
-        return res;
+        return new ArrayList<>(map.values());
     }
 }
