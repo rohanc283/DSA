df25d2a5ea9a9c2dfdc2dc2397beabf4bed0bb78
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-03 16:22:43 +0530

    Time: 23 ms (70.97%) | Memory: 92 MB (6.45%) - LeetSync

---
+++ 2435-shortest-impossible-sequence-of-rolls/shortest-impossible-sequence-of-rolls.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int shortestSequence(int[] rolls, int k) {
+        Set<Integer> set = new HashSet<>();
+        int ans = 0;
+        for (int roll : rolls) {
+            set.add(roll);
+            if (set.size() == k) {
+                set.clear();
+                ans++;
+            }
+        }
+        return ans + 1;
+    }
+}
