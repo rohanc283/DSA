52787d5e000f4bc782566ec4e07719dede6874a3
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-04 12:46:40 +0530

    Time: 4 ms (99.87%) | Memory: 46.8 MB (77.99%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.java
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -1,18 +1,18 @@
 class Solution {
     public int partitionString(String s) {
-        int[] freq = new int[26];
-        int j = 0, n = s.length();
-        int partition = 1;
-        while (j < n) {
-            int idx = s.charAt(j) - 'a';
-            freq[idx]++;
-            if (freq[idx] > 1) {
-                partition++;
-                Arrays.fill(freq, 0);
-                freq[idx]++;
+        int mask = 0;
+        int partitions = 1;
+
+        for (char c : s.toCharArray()) {
+            int bit = 1 << (c - 'a');
+
+            if ((mask & bit) != 0) {
+                partitions++;
+                mask = 0;
             }
-            j++;
+
+            mask |= bit;
         }
-        return partition;
+        return partitions;
     }
 }
