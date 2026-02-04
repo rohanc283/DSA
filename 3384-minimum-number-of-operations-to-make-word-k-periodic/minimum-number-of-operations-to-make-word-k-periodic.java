206c6072e6100cfd85479b71a645caa65c8c0ebd
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-04 14:48:43 +0530

    Time: 39 ms (36.92%) | Memory: 47.9 MB (83.08%) - LeetSync

--- 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.java
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.java
@@ -1,39 +1,18 @@
 class Solution {
     public int minimumOperationsToMakeKPeriodic(String word, int k) {
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < word.length(); i++) {
-            StringBuilder curr = new StringBuilder();
-            int j = i;
-            while (j < word.length() && j < i + k) {
-                curr.append(word.charAt(j++));
-            }
-            String key = curr.toString();
-            map.put(key, map.getOrDefault(key, 0) + 1);
-            j--;
-            i = j;
-        }
 
-        String sr = "";
-        int occ = 0;
-        for (String key : map.keySet()) {
-            int val = map.get(key);
-            if (occ == 0) {
-                occ = val;
-                sr = key;
-            } else {
-                if (val > occ) {
-                    occ = val;
-                    sr = key;
-                }
-            }
-
-        }
-        System.out.println(sr);
-        int count = 0;
         for (int i = 0; i < word.length(); i += k) {
-            if (!word.substring(i, i + k).equals(sr))
-                count++;
+            String block = word.substring(i, i + k);
+            map.put(block, map.getOrDefault(block, 0) + 1);
         }
-        return count;
+
+        int maxFreq = 0;
+        for (int freq : map.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+
+        int totalBlocks = word.length() / k;
+        return totalBlocks - maxFreq;
     }
 }
