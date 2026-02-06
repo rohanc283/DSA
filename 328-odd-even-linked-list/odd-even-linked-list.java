5d951daf50e06c234cfd7878cf51a1b867a92ddc
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-06 19:12:20 +0530

    Time: 1 ms (8.17%) | Memory: 46.8 MB (13.01%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -1,42 +1,42 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode oddHead = null, evenHead = null, oddTail = null, evenTail = null, curr = head;
-        int i = 1;
+        if (head == null)
+            return null;
+
+        ListNode oddHead = null, oddTail = null;
+        ListNode evenHead = null, evenTail = null;
+
+        ListNode curr = head;
+        int index = 1;
+
         while (curr != null) {
             ListNode next = curr.next;
             curr.next = null;
-            if (i % 2 == 0) {
-                if (evenHead == null) {
-                    evenHead = evenTail = curr;
-                } else {
-                    evenTail.next = curr;
-                    evenTail = evenTail.next;
-                }
-            } else {
+
+            if ((index & 1) == 1) {
                 if (oddHead == null) {
                     oddHead = oddTail = curr;
                 } else {
                     oddTail.next = curr;
-                    oddTail = oddTail.next;
+                    oddTail = curr;
+                }
+            } else {
+                if (evenHead == null) {
+                    evenHead = evenTail = curr;
+                } else {
+                    evenTail.next = curr;
+                    evenTail = curr;
                 }
             }
-            i++;
+
             curr = next;
+            index++;
         }
 
-        if (oddTail != null)
+        if (oddTail != null) {
             oddTail.next = evenHead;
-        return oddHead;
+        }
 
+        return oddHead;
     }
 }
