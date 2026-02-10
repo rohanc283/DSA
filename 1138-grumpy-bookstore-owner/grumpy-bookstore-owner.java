f1db58a37f2fb02b9f8dee8d79eebf2b2d05f543
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-10 16:33:22 +0530

    Time: 134 ms (7.54%) | Memory: 48.8 MB (36.70%) - LeetSync

---
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int satisfiedCustomers = 0;
+        int n = customers.length;
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0)
+                satisfiedCustomers += customers[i];
+        }
+
+        int maximumSatisfiedCustomers = satisfiedCustomers;
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            if (grumpy[j] == 1) {
+                satisfiedCustomers += customers[j];
+            }
+
+            if (j >= minutes - 1) {
+                System.out.println(j + " " + satisfiedCustomers);
+                maximumSatisfiedCustomers = Math.max(maximumSatisfiedCustomers, satisfiedCustomers);
+                if (grumpy[i] == 1) {
+                    satisfiedCustomers -= customers[i];
+                }
+                i++;
+            }
+        }
+        return maximumSatisfiedCustomers;
+    }
+}
