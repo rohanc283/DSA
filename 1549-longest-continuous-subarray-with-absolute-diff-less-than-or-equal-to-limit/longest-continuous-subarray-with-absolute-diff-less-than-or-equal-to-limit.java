48b85bf212338a36adcf3f5691b6d16be2e036ff
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-10 15:24:52 +0530

    Time: 32 ms (81.78%) | Memory: 87.1 MB (72.96%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,15 +1,26 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Deque<Integer> minDq = new ArrayDeque<>();
+        Deque<Integer> maxDq = new ArrayDeque<>();
+
         int n = nums.length;
-        int s = 0, e = 0;
+        int e = 0, s = 0;
         int res = 0;
         while (e < n) {
-            map.put(nums[e], map.getOrDefault(nums[e], 0) + 1);
-            while (!map.isEmpty() && map.lastKey() - map.firstKey() > limit) {
-                map.put(nums[s], map.get(nums[s]) - 1);
-                if (map.get(nums[s]) == 0)
-                    map.remove(nums[s]);
+            while (!minDq.isEmpty() && minDq.peekLast() > nums[e]) {
+                minDq.removeLast();
+            }
+            while (!maxDq.isEmpty() && maxDq.peekLast() < nums[e]) {
+                maxDq.removeLast();
+            }
+            minDq.addLast(nums[e]);
+            maxDq.addLast(nums[e]);
+
+            while (maxDq.peekFirst() - minDq.peekFirst() > limit) {
+                if (nums[s] == minDq.peekFirst())
+                    minDq.removeFirst();
+                if (nums[s] == maxDq.peekFirst())
+                    maxDq.removeFirst();
                 s++;
             }
             res = Math.max(res, e - s + 1);
