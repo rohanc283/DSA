57428ef9a7bd150861d16989e23d50c516821b92
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-11 19:15:38 +0530

    Time: 2 ms (96.74%) | Memory: 44.5 MB (95.59%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestValidParentheses(String s) {
+    public int sol1(String s) {
         Deque<Integer> stack = new ArrayDeque<>();
         stack.push(-1);
 
@@ -19,5 +19,43 @@ class Solution {
         }
 
         return maxLen;
+    }
+
+    public int sol2(String s) {
+        int left = 0, right = 0, max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(')
+                left++;
+            else
+                right++;
+
+            if (left == right) {
+                max = Math.max(max, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
+            }
+        }
+
+        left = right = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '(')
+                left++;
+            else
+                right++;
+
+            if (left == right) {
+                max = Math.max(max, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+
+        return max;
+    }
+
+    public int longestValidParentheses(String s) {
+        return sol2(s);
     }
 }
