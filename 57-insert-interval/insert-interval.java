e20afe1a8916925bacb09672b5f521c1493a0f5d
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-13 11:58:08 +0530

    Time: 4 ms (9.92%) | Memory: 47.1 MB (74.21%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -16,7 +16,7 @@ class Solution {
         return res;
     }
 
-    public int[][] insert(int[][] intervals, int[] newInterval) {
+    public int[][] sol1(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
         int[][] newIntervals = new int[n + 1][];
         for (int i = 0; i < n; i++) {
@@ -36,5 +36,31 @@ class Solution {
             i++;
         }
         return buildOutput(res);
+    }
+
+    public int[][] sol2(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+        if (n == 0)
+            return new int[][] { newInterval };
+        List<int[]> res = new ArrayList<>();
+        int i = 0;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i++]);
+        }
+        res.add(newInterval);
+        while (i < n) {
+            if (res.get(res.size() - 1)[1] < intervals[i][0]) {
+                res.add(intervals[i]);
+            } else {
+                res.get(res.size() - 1)[0] = Math.min(res.get(res.size() - 1)[0], intervals[i][0]);
+                res.get(res.size() - 1)[1] = Math.max(res.get(res.size() - 1)[1], intervals[i][1]);
+            }
+            i++;
+        }
+        return buildOutput(res);
+    }
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        return sol2(intervals, newInterval);
     }
 }
