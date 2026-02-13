c7821dae2149a7995a736e829c9af44c8fd8ac39
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-13 11:21:17 +0530

    Time: 9 ms (38.65%) | Memory: 49.3 MB (38.21%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void sortIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+    }
+
+    public int[][] buildOutput(List<int[]> list) {
+        int[][] res = new int[list.size()][2];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public int[][] merge(int[][] intervals) {
+        sortIntervals(intervals);
+        List<int[]> res = new ArrayList<>();
+        int i = 0, n = intervals.length;
+        res.add(intervals[i++]);
+        while (i < n && res.get(res.size() - 1)[1] < intervals[i][0]) {
+            res.add(intervals[i++]);
+        }
+        if (i == n) {
+            return intervals;
+        }
+        while (i < n) {
+            if (res.get(res.size() - 1)[1] < intervals[i][0]) {
+                res.add(intervals[i]);
+            } else {
+                res.get(res.size() - 1)[1] = Math.max(res.get(res.size() - 1)[1], intervals[i][1]);
+            }
+            i++;
+        }
+        while (i < n) {
+            res.add(intervals[i++]);
+        }
+        return buildOutput(res);
+    }
+}
