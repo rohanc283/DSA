7dad3444d24634b4465883314dae73bd87c7ae27
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-20 11:35:45 +0530

    Time: 49 ms (28.12%) | Memory: 86.9 MB (12.75%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.java
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.java
@@ -1,19 +1,58 @@
+
 class Solution {
-    public int maximumScore(int[] nums, int k) {
-        int i = k, j = k, n = nums.length;
-        int res = nums[k], currMin = nums[k];
-        while (i > 0 || j < n - 1) {
-            int leftVal = i > 0 ? nums[i - 1] : 0;
-            int rightVal = j < n - 1 ? nums[j + 1] : 0;
-            if (leftVal < rightVal) {
-                j++;
-                currMin = Math.min(currMin, nums[j]);
-            } else {
-                i--;
-                currMin = Math.min(currMin, nums[i]);
+    public int maximumScoreStackSol(int[] nums, int k) {
+        int n = nums.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
+                stack.pop();
             }
-            res = Math.max(res, currMin * (j - i + 1));
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] >= nums[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (left[i] < k && right[i] > k) {
+                int width = right[i] - left[i] - 1;
+                res = Math.max(res, nums[i] * width);
+            }
         }
         return res;
+    }
+
+    public int maximumScore(int[] nums, int k) {
+        // int i = k, j = k, n = nums.length;
+        // int res = nums[k], currMin = nums[k];
+        // while (i > 0 || j < n - 1) {
+        //     int leftVal = i > 0 ? nums[i - 1] : 0;
+        //     int rightVal = j < n - 1 ? nums[j + 1] : 0;
+        //     if (leftVal < rightVal) {
+        //         j++;
+        //         currMin = Math.min(currMin, nums[j]);
+        //     } else {
+        //         i--;
+        //         currMin = Math.min(currMin, nums[i]);
+        //     }
+        //     res = Math.max(res, currMin * (j - i + 1));
+        // }
+        // return res;
+        return maximumScoreStackSol(nums, k);
     }
 }
