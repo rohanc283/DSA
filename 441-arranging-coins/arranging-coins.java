5cc9911b1cd8c4bc3307099c7947dfe2c01adf87
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-21 10:09:05 +0530

    Time: 1 ms (99.95%) | Memory: 43.2 MB (8.71%) - LeetSync

--- 441-arranging-coins/arranging-coins.java
+++ 441-arranging-coins/arranging-coins.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int arrangeCoins(int n) {
+    public int sol1(int n) {
         int res = 0;
         int stairs = n % 2 == 0 ? n / 2 : n / 2 + 1;
         for (int i = 1; i <= stairs; i++) {
@@ -11,5 +11,21 @@ class Solution {
             n -= i;
         }
         return res;
+    }
+
+    public int arrangeCoins(int n) {
+        long res = 0;
+        long s = 1, e = n % 2 == 0 ? n / 2 : n / 2 + 1;
+        while (s <= e) {
+            long m = s + (e - s) / 2;
+            long sum = (m * (m + 1)) / 2;
+            if (sum <= n) {
+                res = m;
+                s = m + 1;
+            } else {
+                e = m - 1;
+            }
+        }
+        return (int) res;
     }
 }
