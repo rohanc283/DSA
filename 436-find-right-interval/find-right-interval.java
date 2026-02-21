7b55fd73df6088e7be196c72fb7d99e5419ba54e
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-21 11:18:56 +0530

    Time: 196 ms (14.96%) | Memory: 57.4 MB (9.97%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.java
@@ -0,0 +1,46 @@
+class Solution {
+    class Interval {
+        public int start;
+        public int end;
+        public int pos;
+
+        Interval(int start, int end, int pos) {
+            this.start = start;
+            this.end = end;
+            this.pos = pos;
+        }
+    }
+
+    public int findNextInterval(Interval[] intervals, Interval curr, int s, int e) {
+        int res = -1;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            Interval midInterval = intervals[m];
+            if (midInterval.start >= curr.end) {
+                res = midInterval.pos;
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        System.out.println(res);
+        return res;
+
+    }
+
+    public int[] findRightInterval(int[][] intervals) {
+        int n = intervals.length;
+        Interval[] allIntervals = new Interval[n];
+        for (int i = 0; i < n; i++) {
+            allIntervals[i] = new Interval(intervals[i][0], intervals[i][1], i);
+        }
+        Arrays.sort(allIntervals, (a, b) -> a.start - b.start);
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            Interval curr = allIntervals[i];
+            System.out.println(curr.start + " " + curr.end + " " + curr.pos);
+            res[curr.pos] = findNextInterval(allIntervals, curr, i, n - 1);
+        }
+        return res;
+    }
+}
