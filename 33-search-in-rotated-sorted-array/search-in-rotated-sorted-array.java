16b0bbb208ada190fc1f6b51f9412152b36d3620
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-22 15:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (26.11%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -13,7 +13,7 @@ class Solution {
         return l;
     }
 
-    public int search(int[] nums, int target) {
+    public int sol1(int[] nums, int target) {
         int n = nums.length;
         int peakIdx = findMin(nums);
         System.out.println(peakIdx);
@@ -27,5 +27,34 @@ class Solution {
         if (idx < 0)
             return -1;
         return idx;
+    }
+
+    public int sol2(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, h = n - 1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] == target)
+                return m;
+
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    h = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[m] < target && target <= nums[h]) {
+                    l = m + 1;
+                } else {
+                    h = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int search(int[] nums, int target) {
+        return sol2(nums, target);
     }
 }
