8e81ea29948e81e6231232b937a2d67d3550270d
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-22 12:52:30 +0530

    Time: 6 ms (47.37%) | Memory: 82.9 MB (56.75%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,4 +1,25 @@
 class Solution {
+    public int sol2(int[] nums) {
+        int low = 1, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid)
+                    count++;
+            }
+
+            if (count > mid)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+
     void swap(int[] nums, int i, int j) {
         nums[i] ^= nums[j];
         nums[j] ^= nums[i];
