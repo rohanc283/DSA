b1c3c0bc695202f6abd281b26212f34311c48bc7
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-22 16:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (5.05%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,67 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+
+class Solution {
+    public int peakIndexInMountainArray(MountainArray mountainArr) {
+        int n = mountainArr.length();
+        int l = 0, h = n - 1;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (mountainArr.get(m) < mountainArr.get(m + 1)) {
+                l = m + 1;
+            } else {
+                h = m;
+            }
+        }
+        return l;
+    }
+
+    public int leftBinarySearch(int target, MountainArray mountainArr, int l, int h) {
+        int res = -1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (mountainArr.get(m) >= target) {
+                if (mountainArr.get(m) == target)
+                    res = m;
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+
+    public int rightBinarySearch(int target, MountainArray mountainArr, int l, int h) {
+        int res = -1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (mountainArr.get(m) >= target) {
+                if (mountainArr.get(m) == target)
+                    res = m;
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+        return res;
+    }
+
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n = mountainArr.length();
+        int res = -1;
+        int peakIdx = peakIndexInMountainArray(mountainArr);
+        int l = leftBinarySearch(target, mountainArr, 0, peakIdx);
+        if (l >= 0)
+            return l;
+        int r = rightBinarySearch(target, mountainArr, peakIdx, n - 1);
+        if (r >= 0)
+            return r;
+        return -1;
+    }
+}
