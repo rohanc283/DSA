a25934b0ffd4c71afceca96a3f1b6178954c0b7a
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-23 12:13:55 +0530

    Time: 9 ms (37.27%) | Memory: 65.9 MB (5.94%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findSum(int[][] mat, int r1, int c1, int k) {
+        int r2 = r1 + k, c2 = c1 + k;
+        int sum = mat[r2][c2];
+        if (r1 > 0) {
+            sum -= mat[r1 - 1][c2];
+        }
+        if (c1 > 0) {
+            sum -= mat[r2][c1 - 1];
+        }
+        if (r1 > 0 && c1 > 0) {
+            sum += mat[r1 - 1][c1 - 1];
+        }
+        return sum;
+    }
+
+    public int maxSideLength(int[][] mat, int threshold) {
+        int r = mat.length, c = mat[0].length;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                int top = i > 0 ? mat[i - 1][j] : 0;
+                int left = j > 0 ? mat[i][j - 1] : 0;
+                int diag = i > 0 && j > 0 ? mat[i - 1][j - 1] : 0;
+                mat[i][j] += (top + left - diag);
+            }
+        }
+        int best = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                for (int k = best; k < Math.min(r - i, c - j); k++) {
+                    int sum = findSum(mat, i, j, k);
+                    if (sum <= threshold) {
+                        best = Math.max(best, k + 1);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return best;
+    }
+}
