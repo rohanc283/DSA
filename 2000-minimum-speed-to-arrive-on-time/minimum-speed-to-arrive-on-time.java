b263566d98120a5bc0abaeb98dadd55dcf91d722
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-23 12:50:03 +0530

    Time: 208 ms (5.02%) | Memory: 99.6 MB (23.47%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPoss(int[] dist, double hour, int speed) {
+        double currHour = 0;
+        for (int i = 0; i < dist.length; i++) {
+            currHour = Math.ceil(currHour);
+            double time = ((double) dist[i] / (double) speed);
+            currHour += time;
+        }
+        return currHour <= hour;
+    }
+
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int l = 1, h = 10000000;
+        System.out.println(h);
+        int res = -1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (isPoss(dist, hour, m)) {
+                res = m;
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+}
