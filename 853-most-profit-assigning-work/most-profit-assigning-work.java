ad048b2ecb41708740465b81f3db10270dfd4582
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-23 10:35:50 +0530

    Time: 23 ms (53.88%) | Memory: 48.2 MB (88.01%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -29,6 +29,26 @@ class Solution {
         return res;
     }
 
+    public int sol2(int[] difficulty, int[] profit, int[] worker) {
+        int totalJobs = difficulty.length;
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < totalJobs; i++) {
+            jobs.add(new Job(difficulty[i], profit[i]));
+        }
+        Collections.sort(jobs, (a, b) -> a.profit - b.profit);
+        Arrays.sort(worker);
+        int res = 0;
+        int jobIdx = totalJobs - 1;
+        for (int i = worker.length - 1; i >= 0; i--) {
+            while (jobIdx >= 0 && jobs.get(jobIdx).difficulty > worker[i]) {
+                jobIdx--;
+            }
+            int currProfit = jobIdx < 0 ? 0 : jobs.get(jobIdx).profit;
+            res += currProfit;
+        }
+        return res;
+    }
+
     public int upperBound(List<Job> jobs, int tar) {
         int n = jobs.size();
         int l = 0, h = n - 1;
@@ -43,24 +63,27 @@ class Solution {
         return l;
     }
 
+    public int sol3(int[] difficulty, int[] profit, int[] worker) {
+        int totalJobs = difficulty.length;
+        List<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < totalJobs; i++) {
+            jobs.add(new Job(difficulty[i], profit[i]));
+        }
+        Collections.sort(jobs, (a, b) -> a.difficulty - b.difficulty);
+        int res = 0;
+        Arrays.sort(profit);
+        for (int w : worker) {
+            int upperBoundIdx = upperBound(jobs, w);
+            int maxProfit = 0;
+            for (int i = 0; i < upperBoundIdx; i++) {
+                maxProfit = Math.max(maxProfit, jobs.get(i).profit);
+            }
+            res += maxProfit;
+        }
+        return res;
+    }
+
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        // int totalJobs = difficulty.length;
-        // List<Job> jobs = new ArrayList<>();
-        // for (int i = 0; i < totalJobs; i++) {
-        //     jobs.add(new Job(difficulty[i], profit[i]));
-        // }
-        // Collections.sort(jobs, (a, b) -> a.difficulty - b.difficulty);
-        // int res = 0;
-        // Arrays.sort(profit);
-        // for (int w : worker) {
-        //     int upperBoundIdx = upperBound(jobs, w);
-        //     int maxProfit = 0;
-        //     for (int i = 0; i < upperBoundIdx; i++) {
-        //         maxProfit = Math.max(maxProfit, jobs.get(i).profit);
-        //     }
-        //     res += maxProfit;
-        // }
-        // return res;
-        return sol1(difficulty, profit, worker);
+        return sol2(difficulty, profit, worker);
     }
 }
