e19b318f78afe98e3166b7f6a20c7febaea3cf48
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-25 16:03:43 +0530

    Time: 81 ms (5.04%) | Memory: 49 MB (52.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -27,7 +27,67 @@ class Solution {
         return (temp[t / 2] + temp[(t / 2) - 1]) / 2.0;
     }
 
+    public double sol2(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int t = m + n;
+        int idx1 = t / 2, idx2 = (t / 2) - 1;
+        int element1 = -1, element2 = -1;
+
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                if (k == idx1) {
+                    element1 = nums1[i];
+                }
+                if (k == idx2) {
+                    element2 = nums1[i];
+                }
+                i++;
+                k++;
+            } else {
+                if (k == idx1) {
+                    element1 = nums2[j];
+                }
+                if (k == idx2) {
+                    element2 = nums2[j];
+                }
+                j++;
+                k++;
+            }
+
+            System.out.println(element1 + " " + element2);
+        }
+
+        while (i < m) {
+            if (k == idx1) {
+                element1 = nums1[i];
+            }
+            if (k == idx2) {
+                element2 = nums1[i];
+            }
+            i++;
+            k++;
+        }
+
+        while (j < n) {
+            if (k == idx1) {
+                element1 = nums2[j];
+            }
+            if (k == idx2) {
+                element2 = nums2[j];
+            }
+            j++;
+            k++;
+        }
+
+        if (t % 2 != 0) {
+            return element1;
+        }
+
+        return (element1 + element2) / 2.0;
+    }
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return sol1(nums1, nums2);
+        return sol2(nums1, nums2);
     }
 }
