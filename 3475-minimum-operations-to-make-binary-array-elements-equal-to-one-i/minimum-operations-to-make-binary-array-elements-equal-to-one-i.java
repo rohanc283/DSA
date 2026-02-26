c55b44e6d87694173631f6fc701f33937abe4446
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-26 20:57:26 +0530

    Time: 19 ms (5.01%) | Memory: 102 MB (95.93%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int sol1(int[] nums, int k) {
+        int n = nums.length;
+        int flips = 0;
+        int flipCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (i - k >= 0 && nums[i - k] == 5) {
+                flipCount--;
+            }
+
+            if (flipCount % 2 == nums[i]) {
+                if (i + k > n)
+                    return -1;
+                flipCount++;
+                flips++;
+                nums[i] = 5;
+            }
+
+        }
+        return flips;
+    }
+
+    public int sol2(int[] nums, int k) {
+        int n = nums.length;
+        int flips = 0;
+        int flipCount = 0;
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (i - k >= 0) {
+                flipCount -= queue.getFirst();
+                queue.removeFirst();
+            }
+
+            if (flipCount % 2 == nums[i]) {
+                if (i + k > n)
+                    return -1;
+                flipCount++;
+                flips++;
+                queue.addLast(1);
+            } else {
+                queue.addLast(0);
+            }
+        }
+        return flips;
+    }
+
+    public int minOperations(int[] nums) {
+        return sol2(nums, 3);
+    }
+}
