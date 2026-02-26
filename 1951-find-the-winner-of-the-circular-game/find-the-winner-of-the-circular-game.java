f2db4a8448dd3e3c33c4532361b0248db9d65950
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-26 19:28:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (46.38%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findTheWinner(int n, int k) {
+    public int sol1(int n, int k) {
         Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 1; i <= n; i++)
             queue.offer(i);
@@ -12,5 +12,24 @@ class Solution {
             queue.poll();
         }
         return queue.peek();
+    }
+
+    public int findWinnerIdx(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+
+        int idx = findWinnerIdx(n - 1, k);
+        idx = (idx + k) % n;
+        return idx;
+    }
+
+    public int sol2(int n, int k) {
+        int winnerIdx = findWinnerIdx(n, k);
+        return winnerIdx + 1;
+    }
+
+    public int findTheWinner(int n, int k) {
+        return sol2(n, k);
     }
 }
