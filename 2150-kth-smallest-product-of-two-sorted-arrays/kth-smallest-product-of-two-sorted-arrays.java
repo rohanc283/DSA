8714db57e9642df9d6498790533fdfbeff035d44
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-26 09:29:49 +0530

    Time: 809 ms (12.08%) | Memory: 63.2 MB (13.42%) - LeetSync

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/kth-smallest-product-of-two-sorted-arrays.java
@@ -0,0 +1,53 @@
+class Solution {
+    public long countSmallestProduct(int[] nums1, int[] nums2, long midProduct) {
+        int n1 = nums1.length, n2 = nums2.length;
+        long productsCount = 0;
+        for (int i = 0; i < n1; i++) {
+            long num = 1L * nums1[i];
+            if (num >= 0) {
+                int l = 0, h = n2 - 1;
+                int m = -1;
+                while (l <= h) {
+                    int mid = l + (h - l) / 2;
+                    if (num * nums2[mid] <= midProduct) {
+                        m = mid;
+                        l = mid + 1;
+                    } else {
+                        h = mid - 1;
+                    }
+                }
+                productsCount += (m + 1);
+            } else {
+                int l = 0, h = n2 - 1;
+                int m = n2;
+                while (l <= h) {
+                    int mid = l + (h - l) / 2;
+                    if (num * nums2[mid] <= midProduct) {
+                        m = mid;
+                        h = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                }
+                productsCount += (n2 - m);
+            }
+        }
+        return productsCount;
+    }
+
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long l = -10_000_000_000L, h = 10_000_000_000L;
+        long res = 0;
+        while (l <= h) {
+            long midProduct = l + (h - l) / 2;
+            long countSmallest = countSmallestProduct(nums1, nums2, midProduct);
+            if (countSmallest >= k) {
+                res = midProduct;
+                h = midProduct - 1;
+            } else {
+                l = midProduct + 1;
+            }
+        }
+        return res;
+    }
+}
