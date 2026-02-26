1d89f8d950e9b5fa191d5a829ed7b58a11652988
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-26 20:05:11 +0530

    Time: 7 ms (94.18%) | Memory: 45.6 MB (76.77%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -21,11 +21,19 @@ class Solution {
     public int[] sol2(int[] deck) {
         int n = deck.length;
         int[] res = new int[n];
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++)
+            queue.offer(i);
+        for (int i = 0; i < n; i++) {
+            res[queue.poll()] = deck[i];
+            if (!queue.isEmpty())
+                queue.offer(queue.poll());
+        }
         return res;
     }
 
     public int[] deckRevealedIncreasing(int[] deck) {
         Arrays.sort(deck);
-        return sol1(deck);
+        return sol2(deck);
     }
 }
