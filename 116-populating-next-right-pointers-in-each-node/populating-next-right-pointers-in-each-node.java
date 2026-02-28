750e0be1ddd827ac5f2298a9d7492668b815f488
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-28 18:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (55.29%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -22,7 +22,7 @@ class Node {
 */
 
 class Solution {
-    public Node connect(Node root) {
+    public Node sol1(Node root) {
         if (root == null)
             return null;
         Queue<Node> queue = new ArrayDeque<>();
@@ -41,5 +41,27 @@ class Solution {
             }
         }
         return root;
+    }
+
+    public Node sol2(Node root) {
+        if (root == null)
+            return root;
+        Node leftMost = root;
+        while (leftMost.left != null) {
+            Node curr = leftMost;
+            while (curr != null) {
+                curr.left.next = curr.right;
+                if (curr.next != null) {
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+
+    public Node connect(Node root) {
+        return sol2(root);
     }
 }
