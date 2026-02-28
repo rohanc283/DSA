0ebc26d17373ac0b8e877cd19d3b2b1d4327a1ea
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-28 13:33:34 +0530

    Time: 12 ms (81.94%) | Memory: 44.2 MB (63.61%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -29,7 +29,7 @@ class Solution {
         fillParentNodes(root.right, root);
     }
 
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    private List<Integer> sol1(TreeNode root, TreeNode target, int k) {
         List<Integer> res = new ArrayList<>();
         if (root == null || target == null)
             return res;
@@ -43,7 +43,10 @@ class Solution {
             TreeNode currNode = front.node;
             if (currDistance == k) {
                 res.add(currNode.val);
+                continue;
             }
+            if (currDistance > k)
+                continue;
             TreeNode parent = parentMap.get(currNode);
             TreeNode leftChild = currNode.left;
             TreeNode rightChild = currNode.right;
@@ -64,5 +67,54 @@ class Solution {
             }
         }
         return res;
+    }
+
+    private List<Integer> sol2(TreeNode root, TreeNode target, int k) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null || target == null)
+            return res;
+        fillParentNodes(root, null);
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(target);
+        visitedNodes.add(target);
+        int distance = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            if (distance == k) {
+                for (TreeNode node : queue) {
+                    res.add(node.val);
+                }
+                break;
+            }
+
+            for (int i = 0; i < size; i++) {
+                TreeNode front = queue.poll();
+                TreeNode parent = parentMap.get(front);
+                TreeNode leftChild = front.left;
+                TreeNode rightChild = front.right;
+
+                if (parent != null && !visitedNodes.contains(parent)) {
+                    queue.offer(parent);
+                    visitedNodes.add(parent);
+                }
+
+                if (leftChild != null && !visitedNodes.contains(leftChild)) {
+                    queue.offer(leftChild);
+                    visitedNodes.add(leftChild);
+                }
+
+                if (rightChild != null && !visitedNodes.contains(rightChild)) {
+                    queue.offer(rightChild);
+                    visitedNodes.add(rightChild);
+                }
+            }
+
+            distance++;
+        }
+        return res;
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        return sol2(root, target, k);
     }
 }
