4b011e86e87399a7ac3cc1d93fc0940e91f5637f
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-02-28 11:07:54 +0530

    Time: 1 ms (18.32%) | Memory: 43.4 MB (51.46%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -25,11 +25,53 @@ class Solution {
         preorder(root.right, rootLeafNodeValues);
     }
 
-    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    public boolean sol1(TreeNode root1, TreeNode root2) {
         List<Integer> l1 = new ArrayList<>();
         List<Integer> l2 = new ArrayList<>();
         preorder(root1, l1);
         preorder(root2, l2);
         return l1.equals(l2);
+    }
+
+    private Integer getNextLeaf(Stack<TreeNode> stack) {
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+
+            if (node.left == null && node.right == null) {
+                return node.val;
+            }
+        }
+        return null;
+    }
+
+    public boolean sol2(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        if (root1 != null)
+            stack1.push(root1);
+        if (root2 != null)
+            stack2.push(root2);
+
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+
+            Integer leaf1 = getNextLeaf(stack1);
+            Integer leaf2 = getNextLeaf(stack2);
+
+            if (!leaf1.equals(leaf2)) {
+                return false;
+            }
+        }
+
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        return sol2(root1, root2);
     }
 }
