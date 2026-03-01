0640190a3b4193caee31a9a9d4d575e7a6ac601d
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-01 16:09:47 +0530

    Time: 21 ms (20.87%) | Memory: 48.9 MB (56.06%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -15,44 +15,30 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
         List<Integer> res = new ArrayList<>();
-        while (true) {
+
+        while (root1 != null || root2 != null ||
+                !s1.isEmpty() || !s2.isEmpty()) {
+
             while (root1 != null) {
-                stack1.push(root1);
+                s1.push(root1);
                 root1 = root1.left;
             }
             while (root2 != null) {
-                stack2.push(root2);
+                s2.push(root2);
                 root2 = root2.left;
             }
-
-            TreeNode n1 = null, n2 = null;
-            if (!stack1.isEmpty()) {
-                n1 = stack1.peek();
-                stack1.pop();
-            }
-            if (!stack2.isEmpty()) {
-                n2 = stack2.peek();
-                stack2.pop();
-            }
-            if (n1 == null && n2 == null)
-                break;
-            if (n1 == null && n2 != null) {
-                res.add(n2.val);
-                root2 = n2.right;
-            } else if (n1 != null && n2 == null) {
-                res.add(n1.val);
-                root1 = n1.right;
-            } else if (n1.val < n2.val) {
-                res.add(n1.val);
-                stack2.push(n2);
-                root1 = n1.right;
+            if (s2.isEmpty() ||
+                    (!s1.isEmpty() && s1.peek().val <= s2.peek().val)) {
+                TreeNode node = s1.pop();
+                res.add(node.val);
+                root1 = node.right;
             } else {
-                res.add(n2.val);
-                stack1.push(n1);
-                root2 = n2.right;
+                TreeNode node = s2.pop();
+                res.add(node.val);
+                root2 = node.right;
             }
         }
         return res;
