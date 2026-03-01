0e26e227c989d02a04e4eb874f6c20197cae60d9
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-01 12:59:47 +0530

    Time: 3 ms (54.19%) | Memory: 47 MB (50.96%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -17,15 +17,21 @@ class Solution {
 
     Map<Integer, Integer> freq = new HashMap<>();
 
+    Integer prev = null;
+    int count = 0;
+    int maxCount = 0;
+    List<Integer> result = new ArrayList<>();
+
     public void traverse(TreeNode root) {
-        if (root == null) return;
+        if (root == null)
+            return;
 
         freq.put(root.val, freq.getOrDefault(root.val, 0) + 1);
         traverse(root.left);
         traverse(root.right);
     }
 
-    public int[] findMode(TreeNode root) {
+    public int[] sol1(TreeNode root) {
         traverse(root);
 
         int maxFreq = 0;
@@ -33,13 +39,38 @@ class Solution {
             maxFreq = Math.max(maxFreq, count);
         }
 
-        List<Integer> result = new ArrayList<>();
         for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
             if (entry.getValue() == maxFreq) {
                 result.add(entry.getKey());
             }
         }
 
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    public void inorderTraversal(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorderTraversal(root.left);
+        if (prev == null || prev != root.val) {
+            count = 1;
+        } else {
+            count++;
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            result.clear();
+            result.add(root.val);
+        } else if (count == maxCount) {
+            result.add(root.val);
+        }
+        prev = root.val;
+        inorderTraversal(root.right);
+    }
+
+    public int[] findMode(TreeNode root) {
+        inorderTraversal(root);
         return result.stream().mapToInt(i -> i).toArray();
     }
 }
