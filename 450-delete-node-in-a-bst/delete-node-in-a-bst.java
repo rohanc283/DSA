081de483312a05a5a7f65751fc026876dd8ca03f
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-01 11:05:34 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (90.46%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode helper(TreeNode root) {
+        if (root.left == null)
+            return root.right;
+        if (root.right == null)
+            return root.left;
+        TreeNode right = root.right;
+        TreeNode left = root.left;
+        TreeNode leftRight = left;
+        while (leftRight.right != null)
+            leftRight = leftRight.right;
+        leftRight.right = right;
+        return left;
+    }
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null)
+            return null;
+        TreeNode head = root;
+        TreeNode curr = root;
+        if (curr.val == key) {
+            return helper(root);
+        }
+        while (curr != null) {
+            if (curr.val >= key) {
+                if (curr.left != null && curr.left.val == key) {
+                    curr.left = helper(curr.left);
+                    break;
+                } else {
+                    curr = curr.left;
+                }
+            } else {
+                if (curr.right != null && curr.right.val == key) {
+                    curr.right = helper(curr.right);
+                    break;
+                } else {
+                    curr = curr.right;
+                }
+            }
+        }
+        return head;
+    }
+}
