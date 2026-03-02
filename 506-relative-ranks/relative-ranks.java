2bead1adcf0039405940ae1491b9afc62ed537a1
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-02 09:29:16 +0530

    Time: 10 ms (71.52%) | Memory: 47.3 MB (93.50%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,63 @@
+class Solution {
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
+
+    public void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Extract elements
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
+        }
+    }
+
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        int[] sortedScore = score.clone();
+        heapSort(sortedScore);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(sortedScore[i], i);
+        }
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            int pos = map.get(score[i]);
+            if (pos == n - 1) {
+                res[i] = "Gold Medal";
+            } else if (pos == n - 2) {
+                res[i] = "Silver Medal";
+            } else if (pos == n - 3) {
+                res[i] = "Bronze Medal";
+            } else {
+                res[i] = String.valueOf(n - pos);
+            }
+        }
+        return res;
+    }
+}
