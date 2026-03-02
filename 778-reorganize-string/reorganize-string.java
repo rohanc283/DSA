2235bdbb5f01aef0178d1927d1f3d032359afd18
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-02 13:20:23 +0530

    Time: 1 ms (96.35%) | Memory: 43 MB (82.56%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -34,7 +34,46 @@ class Solution {
         return res.toString();
     }
 
+    public String sol2(String s) {
+        int n = s.length();
+        int[] counts = new int[26];
+        char maxiChar = ' ';
+        for (char c : s.toCharArray()) {
+            counts[c - 'a']++;
+            if (counts[c - 'a'] > (n + 1) / 2)
+                return "";
+
+            if (maxiChar == ' ') {
+                maxiChar = c;
+            }
+
+            if (counts[c - 'a'] > counts[maxiChar - 'a']) {
+                maxiChar = c;
+            }
+        }
+        StringBuilder res = new StringBuilder(s);
+
+        int i = 0;
+        while (counts[maxiChar - 'a'] > 0) {
+            res.setCharAt(i, maxiChar);
+            counts[maxiChar - 'a']--;
+            i += 2;
+        }
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            while (counts[c - 'a'] > 0) {
+                if (i >= n)
+                    i = 1;
+                res.setCharAt(i, c);
+                counts[c - 'a']--;
+                i += 2;
+            }
+        }
+
+        return res.toString();
+    }
+
     public String reorganizeString(String s) {
-        return sol1(s);
+        return sol2(s);
     }
 }
