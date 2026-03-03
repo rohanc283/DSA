406cd20d9205aa7646a2dfd763b6b61235eab1db
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-03 19:57:16 +0530

    Time: 50 ms (30.22%) | Memory: 46.6 MB (27.73%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,5 +1,32 @@
 class Solution {
-    public int nthUglyNumber(int n) {
+    public int sol1(int n) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        HashSet<Long> set = new HashSet<>();
+
+        pq.offer(1L);
+        set.add(1L);
+
+        long curr = 1;
+
+        for (int i = 0; i < n; i++) {
+            curr = pq.poll();
+
+            long n2 = curr * 2;
+            long n3 = curr * 3;
+            long n5 = curr * 5;
+
+            if (set.add(n2))
+                pq.offer(n2);
+            if (set.add(n3))
+                pq.offer(n3);
+            if (set.add(n5))
+                pq.offer(n5);
+        }
+
+        return (int) curr;
+    }
+
+    public int sol2(int n) {
         int[] arr = new int[n + 1];
         arr[1] = 1;
         int i2 = 1, i3 = 1, i5 = 1;
@@ -20,5 +47,9 @@ class Solution {
             }
         }
         return arr[n];
+    }
+
+    public int nthUglyNumber(int n) {
+        return sol1(n);
     }
 }
