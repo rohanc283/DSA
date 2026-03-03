4221ad41108c30d3a8b20232de385b061f976801
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-03 12:44:15 +0530

    Time: 88 ms (47.50%) | Memory: 48.7 MB (93.04%) - LeetSync

---
+++ 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int eatenApples(int[] apples, int[] days) {
+
+        // Min heap sorted by expiry day
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> a[1] - b[1]);
+
+        int n = apples.length;
+        int res = 0;
+        int day = 0;
+
+        while (day < n || !pq.isEmpty()) {
+            if (day < n && apples[day] > 0) {
+                pq.offer(new int[] {
+                        apples[day],
+                        day + days[day] - 1
+                });
+            }
+
+            while (!pq.isEmpty() && pq.peek()[1] < day) {
+                pq.poll();
+            }
+
+            if (!pq.isEmpty()) {
+                int[] top = pq.poll();
+                res++;
+                top[0]--;
+                if (top[0] > 0) {
+                    pq.offer(top);
+                }
+            }
+
+            day++;
+        }
+
+        return res;
+    }
+}
