5f2ec376f2bca2d988e7f6b99be3d1691c9d242d
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-03 19:16:51 +0530

    Time: 1 ms (69.14%) | Memory: 52.3 MB (11.83%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -12,7 +12,39 @@ class Solution {
         return pq.peek();
     }
 
+    public static int upperBound(int[] arr, int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public int sol2(int[][] matrix, int k) {
+        int n = matrix.length;
+        int l = matrix[0][0], h = matrix[n - 1][n - 1];
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                int idx = upperBound(matrix[i], m);
+                count += idx;
+            }
+            if (count < k) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+        return l;
+    }
+
     public int kthSmallest(int[][] matrix, int k) {
-        return sol1(matrix, k);
+        return sol2(matrix, k);
     }
 }
