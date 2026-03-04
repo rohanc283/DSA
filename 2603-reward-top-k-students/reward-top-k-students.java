f6b7e79d9b52e2a4fa42bc04f0623e6cf0148ea8
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-04 10:17:32 +0530

    Time: 75 ms (61.13%) | Memory: 60.6 MB (91.36%) - LeetSync

--- 2603-reward-top-k-students/reward-top-k-students.java
+++ 2603-reward-top-k-students/reward-top-k-students.java
@@ -1,31 +1,34 @@
 class Solution {
-    public List<Integer> topStudents(String[] positive_feedback, String[] negative_feedback, String[] report,
-            int[] student_id, int k) {
-        Set<String> positiveFeedbackSet = new HashSet<>();
-        Set<String> negativeFeedbackSet = new HashSet<>();
-        for (String posFeed : positive_feedback) {
-            positiveFeedbackSet.add(posFeed);
-        }
-        for (String negFeed : negative_feedback) {
-            negativeFeedbackSet.add(negFeed);
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] != b[1] ? b[1] - a[1] : a[0] - b[0]);
+    public List<Integer> topStudents(String[] positive_feedback, String[] negative_feedback,
+            String[] report, int[] student_id, int k) {
+
+        Set<String> pos = new HashSet<>(Arrays.asList(positive_feedback));
+        Set<String> neg = new HashSet<>(Arrays.asList(negative_feedback));
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> a[1] != b[1] ? a[1] - b[1] : b[0] - a[0]);
+
         for (int i = 0; i < report.length; i++) {
             int score = 0;
-            String[] words = report[i].split(" ");
-            for (String word : words) {
-                if (positiveFeedbackSet.contains(word)) {
+            for (String word : report[i].split(" ")) {
+                if (pos.contains(word))
                     score += 3;
-                } else if (negativeFeedbackSet.contains(word)) {
-                    score--;
-                }
+                else if (neg.contains(word))
+                    score -= 1;
             }
+
             pq.offer(new int[] { student_id[i], score });
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
+
         List<Integer> res = new ArrayList<>();
-        while (k-- > 0) {
+        while (!pq.isEmpty()) {
             res.add(pq.poll()[0]);
         }
+
+        Collections.reverse(res);
         return res;
     }
 }
