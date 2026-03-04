61a2063b5a31c03f9ab071ada4f2f7864650f8f1
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-04 11:00:09 +0530

    Time: 35 ms (84.72%) | Memory: 50.4 MB (97.67%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,29 +1,46 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
+
         int n = times.length;
-        int[][] timesWithPos = new int[n][3];
+        int[][] arr = new int[n][3];
+
         for (int i = 0; i < n; i++) {
-            timesWithPos[i][0] = times[i][0];
-            timesWithPos[i][1] = times[i][1];
-            timesWithPos[i][2] = i;
+            arr[i][0] = times[i][0];
+            arr[i][1] = times[i][1];
+            arr[i][2] = i;
         }
-        Arrays.sort(timesWithPos, (a, b) -> Integer.compare(a[0], b[0]));
-        int timer = 0;
-        int chair = 0;
-        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
-        PriorityQueue<int[]> takenChairs = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<Integer> available = new PriorityQueue<>();
+        PriorityQueue<int[]> occupied = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        int nextChair = 0;
+
         for (int i = 0; i < n; i++) {
-            timer = Math.max(timer, timesWithPos[i][0]);
-            availableChairs.offer(chair++);
-            while (!takenChairs.isEmpty() && takenChairs.peek()[0] <= timer) {
-                availableChairs.offer(takenChairs.poll()[1]);
+
+            int arrival = arr[i][0];
+            int leave = arr[i][1];
+
+            while (!occupied.isEmpty() && occupied.peek()[0] <= arrival) {
+                available.offer(occupied.poll()[1]);
             }
-            int takenChair = availableChairs.poll();
-            takenChairs.offer(new int[] { timesWithPos[i][1], takenChair });
-            if (timesWithPos[i][2] == targetFriend) {
-                return takenChair;
+
+            int assignedChair;
+
+            if (available.isEmpty()) {
+                assignedChair = nextChair++;
+            } else {
+                assignedChair = available.poll();
+            }
+
+            occupied.offer(new int[] { leave, assignedChair });
+
+            if (arr[i][2] == targetFriend) {
+                return assignedChair;
             }
         }
-        return 0;
+
+        return -1;
     }
 }
