f50d8c35a82f5f2bdb72f77e1b4c216304a32455
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-04 11:08:36 +0530

    Time: 181 ms (41.00%) | Memory: 51.9 MB (38.50%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
@@ -1,23 +1,31 @@
 class Solution {
     public String clearStars(String s) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] != b[1] ? a[1] - b[1] : b[0] - a[0]);
-        for (int i = 0; i < s.length(); i++) {
+        int n = s.length();
+        boolean[] removed = new boolean[n];
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> a[1] != b[1] ? a[1] - b[1] : b[0] - a[0]);
+
+        for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
+
             if (c == '*') {
-                if (!pq.isEmpty())
-                    pq.poll();
+                int[] removedChar = pq.poll();
+                removed[removedChar[0]] = true;
+                removed[i] = true;
             } else {
                 pq.offer(new int[] { i, c });
             }
         }
-        PriorityQueue<int[]> pq2 = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        while (!pq.isEmpty()) {
-            pq2.offer(pq.poll());
-        }
+
         StringBuilder res = new StringBuilder();
-        while (!pq2.isEmpty()) {
-            res.append((char) pq2.poll()[1]);
+
+        for (int i = 0; i < n; i++) {
+            if (!removed[i] && s.charAt(i) != '*') {
+                res.append(s.charAt(i));
+            }
         }
+
         return res.toString();
     }
 }
