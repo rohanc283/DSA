574e054ed220b0a27584acfe664ae74a9bf62e2d
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-05 17:27:36 +0530

    Time: 76 ms (25.33%) | Memory: 155.9 MB (80.00%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
+        Arrays.sort(horizontalCut);
+        Arrays.sort(verticalCut);
+        int i = horizontalCut.length - 1, j = verticalCut.length - 1;
+        long horizontalPieces = 1, verticalPieces = 1;
+        long cost = 0;
+        while (i >= 0 && j >= 0) {
+            if (horizontalCut[i] > verticalCut[j]) {
+                cost += (horizontalCut[i] * verticalPieces);
+                horizontalPieces++;
+                i--;
+            } else {
+                cost += (verticalCut[j] * horizontalPieces);
+                verticalPieces++;
+                j--;
+            }
+        }
+        while (i >= 0) {
+            cost += (horizontalCut[i] * verticalPieces);
+            horizontalPieces++;
+            i--;
+        }
+        while (j >= 0) {
+            cost += (verticalCut[j] * horizontalPieces);
+            verticalPieces++;
+            j--;
+        }
+        return cost;
+    }
+}
