0250051aa96013a7dfc1a5f8dfecc0cea008e991
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-07 11:20:49 +0530

    Time: 1 ms (86.36%) | Memory: 42.8 MB (94.18%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -24,9 +24,29 @@ class Solution {
         return dp[idx] = ways;
     }
 
+    public int solveTab(String s) {
+        dp = new int[s.length() + 2];
+        int n = s.length();
+        Arrays.fill(dp, 0);
+        dp[n] = 1;
+        dp[n + 1] = 1;
+        for (int idx = n - 1; idx >= 0; idx--) {
+            if (s.charAt(idx) == '0') continue;
+            int ways = dp[idx + 1];
+            if (idx + 1 < n) {
+                int num = (s.charAt(idx) - '0') * 10 + (s.charAt(idx + 1) - '0');
+                if (num <= 26)
+                    ways += dp[idx + 2];
+            }
+            dp[idx] = ways;
+        }
+        return dp[0];
+    }
+
     public int numDecodings(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return solve(s, 0);
+        // dp = new int[s.length()];
+        // Arrays.fill(dp, -1);
+        // return solve(s, 0);
+        return solveTab(s);
     }
 }
