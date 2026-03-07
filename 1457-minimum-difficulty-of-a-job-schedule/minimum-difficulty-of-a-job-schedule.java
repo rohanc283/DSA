d6b0f207b874aec08d1c7e77ac9d3ef529aa020a
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-07 20:23:19 +0530

    Time: 38 ms (8.92%) | Memory: 43.7 MB (25.98%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.java
@@ -1,8 +1,10 @@
 class Solution {
+
     Integer[][] dp;
     int n;
 
     private int solve(int[] difficulty, int i, int d) {
+
         if (d == 1) {
             int max = 0;
             while (i < n) {
@@ -10,22 +12,34 @@ class Solution {
             }
             return max;
         }
+
         if (dp[i][d] != null)
             return dp[i][d];
+
         int max = 0;
         int ans = Integer.MAX_VALUE;
+
         for (int j = i; j <= n - d; j++) {
+
             max = Math.max(max, difficulty[j]);
+
             int curr = max + solve(difficulty, j + 1, d - 1);
+
             ans = Math.min(ans, curr);
         }
+
         return dp[i][d] = ans;
     }
 
     public int minDifficulty(int[] jobDifficulty, int d) {
+
         n = jobDifficulty.length;
+
+        if (n < d)
+            return -1;
+
         dp = new Integer[n][d + 1];
-        int ans = solve(jobDifficulty, 0, d);
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+
+        return solve(jobDifficulty, 0, d);
     }
 }
