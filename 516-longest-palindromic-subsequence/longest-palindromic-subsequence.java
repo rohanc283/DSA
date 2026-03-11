cb52a173da6f4616424d00421d7856eb30a5f730
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-11 19:29:44 +0530

    Time: 58 ms (35.39%) | Memory: 63.7 MB (58.60%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findLCS(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        String s2 = new StringBuilder(s).reverse().toString();
+        return findLCS(s, s2);
+    }
+}
