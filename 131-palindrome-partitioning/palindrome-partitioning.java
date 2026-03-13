d492b99126cc173e4cb3664b3f2a112cebe323fd
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-13 15:20:52 +0530

    Time: 7 ms (97.90%) | Memory: 65.3 MB (54.55%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -12,6 +12,7 @@ class Solution {
     private void solve(String s, int idx, List<String> curr, List<List<String>> res) {
         if (idx >= n) {
             res.add(new ArrayList<>(curr));
+            return;
         }
         for (int i = idx; i < n; i++) {
             if (isPalindrome(s, idx, i)) {
@@ -22,11 +23,56 @@ class Solution {
         }
     }
 
-    public List<List<String>> partition(String s) {
+    private List<List<String>> sol1(String s) {
         List<List<String>> res = new ArrayList<>();
         List<String> curr = new ArrayList<>();
         n = s.length();
         solve(s, 0, curr, res);
         return res;
+    }
+
+    private void solve2(String s, int idx, Boolean[][] dp, List<String> curr, List<List<String>> res) {
+        if (idx >= n) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for (int i = idx; i < n; i++) {
+            if (dp[idx][i]) {
+                curr.add(s.substring(idx, i + 1));
+                solve2(s, i + 1, dp, curr, res);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+
+    private List<List<String>> sol2(String s) {
+        n = s.length();
+        Boolean[][] dp = new Boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], false);
+            dp[i][i] = true;
+        }
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (len == 2) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+        solve2(s, 0, dp, curr, res);
+        return res;
+    }
+
+    public List<List<String>> partition(String s) {
+        return sol2(s);
     }
 }
