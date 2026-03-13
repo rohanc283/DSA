1dfe8d2951fe3808930b6c3590336ba14682a3a3
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-13 12:10:41 +0530

    Time: 13 ms (5.33%) | Memory: 56.3 MB (6.11%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -8,19 +8,14 @@ class Solution {
             return nums[i];
         if (dp[i][j] != null)
             return dp[i][j];
-        long takeI = nums[i] + Math.min(solve(nums, i + 2, j), solve(nums, i + 1, j - 1));
-        long takeJ = nums[j] + Math.min(solve(nums, i + 1, j - 1), solve(nums, i, j - 2));
+        long takeI = nums[i] - solve(nums, i + 1, j);
+        long takeJ = nums[j] - solve(nums, i, j - 1);
         return dp[i][j] = Math.max(takeI, takeJ);
     }
 
     public boolean stoneGame(int[] piles) {
-        int sum = 0;
-        for (int num : piles)
-            sum += num;
         int n = piles.length;
         dp = new Long[n][n];
-        long aliceScore = solve(piles, 0, n - 1);
-        long bobScore = sum - aliceScore;
-        return aliceScore >= bobScore;
+        return solve(piles, 0, n - 1) >= 0;
     }
 }
