228b39f9b5ae8141944c476cc8b8ab92174456f0
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-14 21:44:50 +0530

    Time: 21 ms (25.19%) | Memory: 47 MB (24.43%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,24 +1,34 @@
 class Solution {
-    private void solve(int n, String curr, List<String> result) {
+    int count = 0;
+    int K;
+    String result;
+
+    private void solve(int n, String curr) {
         if (curr.length() == n) {
-            result.add(curr);
+            count++;
+            if (count == K) {
+                result = curr;
+            }
             return;
         }
         for (char ch = 'a'; ch <= 'c'; ch++) {
             if (curr.length() > 0 && curr.charAt(curr.length() - 1) == ch)
                 continue;
-            solve(n, curr + ch, result);
+            solve(n, curr + ch);
         }
     }
 
     private String sol1(int n, int k) {
         String curr = "";
-        List<String> result = new ArrayList<>();
-        solve(n, curr, result);
-        if (result.size() < k) {
-            return "";
-        }
-        return result.get(k - 1);
+        // List<String> result = new ArrayList<>();
+        result = "";
+        K = k;
+        solve(n, curr);
+        // if (result.size() < k) {
+        //     return "";
+        // }
+        // return result.get(k - 1);
+        return result;
     }
 
     public String getHappyString(int n, int k) {
