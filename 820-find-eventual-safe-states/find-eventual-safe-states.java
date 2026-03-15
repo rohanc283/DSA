6a415755ef4e5c1b56182ba082df759cb01d227e
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-15 18:16:57 +0530

    Time: 20 ms (49.69%) | Memory: 60.7 MB (75.85%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -37,7 +37,45 @@ class Solution {
         return safeNodes;
     }
 
+    private List<Integer> sol2(int[][] graph) {
+        int V = graph.length;
+        List<List<Integer>> reversedGraph = new ArrayList<>();
+        for (int i = 0; i < V; i++)
+            reversedGraph.add(new ArrayList<>());
+
+        int[] indegree = new int[V];
+        for (int u = 0; u < graph.length; u++) {
+            for (int v : graph[u]) {
+                reversedGraph.get(v).add(u);
+                indegree[u]++;
+            }
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        boolean[] isSafe = new boolean[V];
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            isSafe[u] = true;
+            for (int nei : reversedGraph.get(u)) {
+                indegree[nei]--;
+                if (indegree[nei] == 0) {
+                    queue.offer(nei);
+                }
+            }
+        }
+        List<Integer> safeNodes = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (isSafe[i])
+                safeNodes.add(i);
+        }
+        return safeNodes;
+    }
+
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        return sol1(graph);
+        return sol2(graph);
     }
 }
