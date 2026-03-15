b7062da39277ccb9b2d671d7e6480ce5d3708202
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-15 14:19:43 +0530

    Time: 334 ms (63.89%) | Memory: 47.2 MB (86.90%) - LeetSync

--- 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.java
+++ 2671-shortest-cycle-in-a-graph/shortest-cycle-in-a-graph.java
@@ -1,37 +1,7 @@
 class Solution {
-    List<List<Integer>> graph;
-
-    private int BFS(int start, int n) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        Map<Integer, Integer> parent = new HashMap<>();
-        int[] dist = new int[n];
-        boolean[] visited = new boolean[n];
-
-        queue.offer(start);
-        visited[start] = true;
-        parent.put(start, -1);
-        dist[start] = 0;
-
-        int ans = Integer.MAX_VALUE;
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            for (int nei : graph.get(curr)) {
-                if (!visited[nei]) {
-                    queue.offer(nei);
-                    visited[nei] = true;
-                    parent.put(nei, curr);
-                    dist[nei] = dist[curr] + 1;
-                } else if (parent.get(curr) != nei) {
-                    ans = Math.min(ans, dist[curr] + dist[nei] + 1);
-                }
-            }
-        }
-        return ans;
-    }
 
     public int findShortestCycle(int n, int[][] edges) {
-        graph = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
 
         for (int i = 0; i < n; i++)
             graph.add(new ArrayList<>());
@@ -41,10 +11,33 @@ class Solution {
             graph.get(e[1]).add(e[0]);
         }
 
-        int res = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            res = Math.min(res, BFS(i, n));
+        int ans = Integer.MAX_VALUE;
+
+        for (int start = 0; start < n; start++) {
+            int[] dist = new int[n];
+            int[] parent = new int[n];
+
+            Arrays.fill(dist, -1);
+
+            Queue<Integer> q = new ArrayDeque<>();
+            q.offer(start);
+            dist[start] = 0;
+            parent[start] = -1;
+
+            while (!q.isEmpty()) {
+                int curr = q.poll();
+
+                for (int nei : graph.get(curr)) {
+                    if (dist[nei] == -1) {
+                        dist[nei] = dist[curr] + 1;
+                        parent[nei] = curr;
+                        q.offer(nei);
+                    } else if (parent[curr] != nei) {
+                        ans = Math.min(ans, dist[curr] + dist[nei] + 1);
+                    }
+                }
+            }
         }
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
