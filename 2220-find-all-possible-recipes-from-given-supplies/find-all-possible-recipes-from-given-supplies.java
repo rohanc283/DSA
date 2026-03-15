83e588554572b5db78216a5fd6728d36de7d60f5
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-15 20:19:26 +0530

    Time: 33 ms (88.74%) | Memory: 49.2 MB (55.82%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+    private List<String> sol1(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         int n = recipes.length;
         Set<String> suppliesSet = new HashSet<>();
         for (String supp : supplies)
@@ -27,5 +27,46 @@ class Solution {
             }
         }
         return result;
+    }
+
+    public List<String> sol2(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        int n = recipes.length;
+        Set<String> suppliesSet = new HashSet<>();
+        for (String supp : supplies)
+            suppliesSet.add(supp);
+
+        Map<String, List<Integer>> graph = new HashMap<>();
+        int[] indegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (String ingredient : ingredients.get(i)) {
+                if (!suppliesSet.contains(ingredient)) {
+                    graph.computeIfAbsent(ingredient, a -> new ArrayList<>()).add(i);
+                    indegree[i]++;
+                }
+            }
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        List<String> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int f = queue.poll();
+            String recipe = recipes[f];
+            result.add(recipe);
+            for (Integer nei : graph.getOrDefault(recipe, new ArrayList<>())) {
+                indegree[nei]--;
+                if (indegree[nei] == 0)
+                    queue.offer(nei);
+            }
+
+        }
+        return result;
+    }
+
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        return sol2(recipes, ingredients, supplies);
     }
 }
