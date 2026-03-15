d09390eb475eddc832bb1e2557706c502daba295
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-15 19:30:29 +0530

    Time: 62 ms (84.08%) | Memory: 78.5 MB (73.79%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -30,9 +30,41 @@ class Solution {
         }
         return result;
     }
-    
+
+    private void DFSSol2(List<List<Integer>> graph, int curr, boolean[] visited) {
+        for (int nei : graph.get(curr)) {
+            if (!visited[nei]) {
+                visited[nei] = true;
+                DFSSol2(graph, nei, visited);
+            }
+        }
+    }
+
+    private List<List<Integer>> sol2(int n, int[][] edges) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+            result.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            graph.get(v).add(u);
+        }
+        for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[n];
+            DFSSol2(graph, i, visited);
+            for (int j = 0; j < n; j++) {
+                if (visited[j]) {
+                    result.get(i).add(j);
+                }
+            }
+        }
+        return result;
+    }
 
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        return sol1(n, edges);
+        return sol2(n, edges);
     }
 }
