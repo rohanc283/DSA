2475bc16498df8deb1af1e974b0c88a6a125c0f3
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-15 13:29:05 +0530

    Time: 9 ms (96.69%) | Memory: 46.3 MB (88.20%) - LeetSync

--- 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
@@ -3,28 +3,36 @@ class Solution {
         int n = graph.length;
         Queue<int[]> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][1 << n];
+
         for (int i = 0; i < n; i++) {
-            queue.offer(new int[] { i, 1 << i });
+            queue.offer(new int[]{i, 1 << i});
             visited[i][1 << i] = true;
         }
+
         int path = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
+
             for (int i = 0; i < size; i++) {
                 int[] curr = queue.poll();
-                int node = curr[0], state = curr[1];
-                if (state == (1 << n) - 1)
+                int node = curr[0];
+                int mask = curr[1];
+
+                if (mask == (1 << n) - 1)
                     return path;
+
                 for (int nei : graph[node]) {
-                    int nextMask = state | (1 << nei);
+                    int nextMask = mask | (1 << nei);
+
                     if (!visited[nei][nextMask]) {
-                        queue.offer(new int[] { nei, nextMask });
                         visited[nei][nextMask] = true;
+                        queue.offer(new int[]{nei, nextMask});
                     }
                 }
             }
             path++;
         }
-        return path;
+
+        return -1;
     }
 }
