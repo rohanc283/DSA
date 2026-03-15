d8f041fdc5b66f679f0ffe48e56f3622538fa479
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-15 14:52:58 +0530

    Time: 1 ms (64.07%) | Memory: 47.3 MB (53.38%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -14,13 +14,33 @@ class Solution {
         return true;
     }
 
+    private boolean BFS(int curr, int currColor, int[] color, int[][] graph) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(curr);
+        color[curr] = currColor;
+        while (!queue.isEmpty()) {
+            int f = queue.poll();
+            for (int nei : graph[f]) {
+                if (color[nei] == color[f]) {
+                    return false;
+                }
+                if (color[nei] == -1) {
+                    int newColor = 1 - color[f];
+                    color[nei] = newColor;
+                    queue.offer(nei);
+                }
+            }
+        }
+        return true;
+    }
+
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int[] color = new int[n];
         Arrays.fill(color, -1);
         for (int i = 0; i < n; i++) {
             if (color[i] == -1) {
-                boolean bipartite = DFS(i, 1, color, graph);
+                boolean bipartite = BFS(i, 1, color, graph);
                 if (!bipartite)
                     return false;
             }
