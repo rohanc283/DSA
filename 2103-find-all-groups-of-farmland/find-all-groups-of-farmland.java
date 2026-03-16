99cb6e867f2217242a3a05cb64f1e8d89cfde367
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-16 17:34:59 +0530

    Time: 38 ms (6.35%) | Memory: 99 MB (42.86%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.java
@@ -16,6 +16,27 @@ class Solution {
         }
     }
 
+    private void BFS(int[][] land, int i, int j, int[] r2, int[] c2) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] { i, j });
+        land[i][j] = 0;
+        while (!queue.isEmpty()) {
+            int[] f = queue.poll();
+            i = f[0];
+            j = f[1];
+            r2[0] = Math.max(r2[0], i);
+            c2[0] = Math.max(c2[0], j);
+            for (int[] d : dir) {
+                int nr = i + d[0];
+                int nc = j + d[1];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && land[nr][nc] == 1) {
+                    queue.offer(new int[] { nr, nc });
+                    land[nr][nc] = 0;
+                }
+            }
+        }
+    }
+
     public int[][] sol1(int[][] land) {
         m = land.length;
         n = land[0].length;
@@ -26,7 +47,7 @@ class Solution {
                     int r1 = i, c1 = j;
                     int[] r2 = new int[1];
                     int[] c2 = new int[1];
-                    DFS(land, i, j, r2, c2);
+                    BFS(land, i, j, r2, c2);
                     result.add(new ArrayList<>(Arrays.asList(r1, c1, r2[0], c2[0])));
                 }
             }
