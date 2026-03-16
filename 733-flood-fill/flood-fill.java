b0f7687623b62c1885f20cb1507596c0077177b1
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-16 14:59:43 +0530

    Time: 1 ms (44.71%) | Memory: 47.1 MB (22.33%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,31 @@
+class Solution {
+    int m, n;
+
+    private void DFS(int[][] image, int sr, int sc, int newColor, boolean[][] visited) {
+        if (sr >= m || sr < 0 || sc < 0 || sc >= n)
+            return;
+        if (visited[sr][sc])
+            return;
+        visited[sr][sc] = true;
+        int currColor = image[sr][sc];
+        image[sr][sc] = newColor;
+        int[][] dir = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        for (int[] d : dir) {
+            int newRow = sr + d[0];
+            int newCol = sc + d[1];
+            if (newRow < 0 || newCol < 0 || newRow >= m || newCol >= n)
+                continue;
+            if (!visited[newRow][newCol] && image[newRow][newCol] == currColor) {
+                DFS(image, newRow, newCol, newColor, visited);
+            }
+        }
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        m = image.length;
+        n = image[0].length;
+        boolean[][] visited = new boolean[n][n];
+        DFS(image, sr, sc, color, visited);
+        return image;
+    }
+}
