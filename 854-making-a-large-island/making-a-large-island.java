ffe6886dd78d93f311dca22c88e2b927f4a63664
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-03-17 18:38:47 +0530

    Time: 83 ms (69.40%) | Memory: 134.4 MB (83.87%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -1,42 +1,65 @@
-class Solution {
+class DSU {
+    int[] parent, size;
 
-    int n;
-    int[][] grid;
-    Map<Integer, Integer> map = new HashMap<>();
+    DSU(int n) {
+        parent = new int[n];
+        size = new int[n];
 
-    private int dfs(int i, int j, int id) {
-        if (i < 0 || j < 0 || i >= n || j >= n || grid[i][j] != 1)
-            return 0;
-
-        grid[i][j] = id;
-
-        int size = 1;
-
-        size += dfs(i + 1, j, id);
-        size += dfs(i - 1, j, id);
-        size += dfs(i, j + 1, id);
-        size += dfs(i, j - 1, id);
-
-        return size;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
     }
 
-    public int largestIsland(int[][] grid) {
-        this.grid = grid;
-        this.n = grid.length;
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
 
-        int id = 2;
-        int max = 0;
+    void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+
+        if (pa == pb)
+            return;
+
+        if (size[pa] < size[pb]) {
+            parent[pa] = pb;
+            size[pb] += size[pa];
+        } else {
+            parent[pb] = pa;
+            size[pa] += size[pb];
+        }
+    }
+}
+
+class Solution {
+    public int largestIsland(int[][] grid) {
+        int n = grid.length;
+        DSU dsu = new DSU(n * n);
+
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+
                 if (grid[i][j] == 1) {
-                    int size = dfs(i, j, id);
-                    map.put(id, size);
-                    max = Math.max(max, size);
-                    id++;
+                    for (int[] d : dirs) {
+                        int ni = i + d[0];
+                        int nj = j + d[1];
+
+                        if (ni >= 0 && nj >= 0 && ni < n && nj < n && grid[ni][nj] == 1) {
+                            int u = i * n + j;
+                            int v = ni * n + nj;
+                            dsu.union(u, v);
+                        }
+                    }
                 }
             }
         }
+
+        int max = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -44,28 +67,31 @@ class Solution {
                 if (grid[i][j] == 0) {
 
                     Set<Integer> seen = new HashSet<>();
-                    int currSize = 1;
-
-                    int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+                    int size = 1;
 
                     for (int[] d : dirs) {
                         int ni = i + d[0];
                         int nj = j + d[1];
 
-                        if (ni >= 0 && nj >= 0 && ni < n && nj < n) {
-                            int islandId = grid[ni][nj];
+                        if (ni >= 0 && nj >= 0 && ni < n && nj < n && grid[ni][nj] == 1) {
+                            int parent = dsu.find(ni * n + nj);
 
-                            if (islandId > 1 && !seen.contains(islandId)) {
-                                currSize += map.get(islandId);
-                                seen.add(islandId);
+                            if (!seen.contains(parent)) {
+                                size += dsu.size[parent];
+                                seen.add(parent);
                             }
                         }
                     }
 
-                    max = Math.max(max, currSize);
+                    max = Math.max(max, size);
                 }
             }
         }
+
+        for (int i = 0; i < n * n; i++) {
+            max = Math.max(max, dsu.size[dsu.find(i)]);
+        }
+
         return max;
     }
 }
