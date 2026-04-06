084815dc1c56e15b773f46317ec163b3c66daa56
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-06 18:06:39 +0530

    Time: 1 ms (99.97%) | Memory: 43.9 MB (27.28%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,20 +1,18 @@
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        char[] digits1 = a.toCharArray();
+        char[] digits2 = b.toCharArray();
+        int n1 = digits1.length, n2 = digits2.length;
         int carry = 0;
-
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int d1 = (i >= 0) ? a.charAt(i--) - '0' : 0;
-            int d2 = (j >= 0) ? b.charAt(j--) - '0' : 0;
-
-            int sumBit = d1 ^ d2 ^ carry;
-            sb.append(sumBit);
-
-            carry = (d1 & d2) | (carry & (d1 ^ d2));
+        StringBuilder res = new StringBuilder();
+        int i = n1 - 1, j = n2 - 1;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int d1 = i < 0 ? 0 : digits1[i--] - '0';
+            int d2 = j < 0 ? 0 : digits2[j--] - '0';
+            int d = carry ^ d1 ^ d2;
+            carry = (carry & (d1 ^ d2)) >= 1 || (d1 & d2) >= 1 ? 1 : 0;
+            res.append((char) (d + '0'));
         }
-
-        return sb.reverse().toString();
+        return res.reverse().toString();
     }
 }
