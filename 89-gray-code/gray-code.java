3e56d0c5630d2b969d7223700eb421cb85d78987
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-07 18:22:53 +0530

    Time: 4 ms (99.15%) | Memory: 53.8 MB (29.58%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,8 +1,7 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         List<Integer> res = new ArrayList<>();
-        int s = 1 << n;
-        for (int i = 0; i < s; i++) {
+        for (int i = 0; i < (1 << n); i++) {
             res.add(i ^ (i >> 1));
         }
         return res;
