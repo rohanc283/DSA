d29d8e987f0b8852a267350d03029728182e1d6e
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 19:23:25 +0530

    Time: 13 ms (18.65%) | Memory: 46.9 MB (35.32%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,28 +1,20 @@
 class Solution {
-    int countBits(int n) {
-        int ans = 0;
-        while (n > 0) {
-            n = (n & n - 1);
-            ans++;
+    private int numberOfSetBits(int num) {
+        int setBits = 0;
+        while (num > 0) {
+            setBits++;
+            num &= (num - 1);
         }
-        return ans;
+        return setBits;
     }
 
     public int[] sortByBits(int[] arr) {
-        Integer[] boxed = Arrays.stream(arr)
-                .boxed()
-                .toArray(Integer[]::new);
-
-        Arrays.sort(boxed, (a, b) -> {
-            int c1 = countBits(a), c2 = countBits(b);
-            if (c1 == c2) {
-                return a - b;
-            }
-            return c1 - c2;
-        });
-
-        return Arrays.stream(boxed)
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return Arrays.stream(arr).boxed().sorted((a, b) -> {
+            int aBit = numberOfSetBits(a);
+            int bBit = numberOfSetBits(b);
+            if (Integer.compare(aBit, bBit) != 0)
+                return Integer.compare(aBit, bBit);
+            return Integer.compare(a, b);
+        }).mapToInt(Integer::intValue).toArray();
     }
 }
