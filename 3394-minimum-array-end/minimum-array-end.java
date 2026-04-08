5f36cdbd9344c34a2eb2eb97c2d5a9245098f8fb
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 17:35:58 +0530

    Time: 1764 ms (5.88%) | Memory: 41.7 MB (99.02%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.java
+++ 3394-minimum-array-end/minimum-array-end.java
@@ -1,7 +1,7 @@
 class Solution {
     public long minEnd(int n, int x) {
         long num = x;
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             num = (num + 1) | x;
         }
         return num;
