a6a6d3022f291522f0b8511ccf6aa7820ca99113
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 13:36:10 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (98.05%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.java
+++ 3192-maximum-xor-product/maximum-xor-product.java
@@ -1,33 +1,35 @@
 class Solution {
     public int maximumXorProduct(long a, long b, int n) {
-        long xOrA = 0, xOrB = 0;
+        long xorA = 0, xorB = 0;
         for (long i = 49; i >= n; i--) {
             boolean a_ith_bit = ((a >> i) & 1) >= 1;
             boolean b_ith_bit = ((b >> i) & 1) >= 1;
             if (a_ith_bit) {
-                xOrA |= (1L << i);
+                xorA |= (1L << i);
             }
             if (b_ith_bit) {
-                xOrB |= (1L << i);
+                xorB |= (1L << i);
             }
         }
-        for (long i = n - 1; i >= 0; i--) {
+
+        for (int i = n - 1; i >= 0; i--) {
             boolean a_ith_bit = ((a >> i) & 1) >= 1;
             boolean b_ith_bit = ((b >> i) & 1) >= 1;
             if (a_ith_bit == b_ith_bit) {
-                xOrA |= (1L << i);
-                xOrB |= (1L << i);
+                xorA |= (1L << i);
+                xorB |= (1L << i);
             } else {
-                if (xOrA > xOrB) {
-                    xOrB |= (1L << i);
+                if (xorA > xorB) {
+                    xorB |= (1L << i);
                 } else {
-                    xOrA |= (1L << i);
+                    xorA |= (1L << i);
                 }
             }
         }
-        xOrA %= 1_000_000_007;
-        xOrB %= 1_000_000_007;
-        int res = (int) ((xOrA * xOrB) % 1_000_000_007);
+        int MOD = 1_000_000_007;
+        xorA %= MOD;
+        xorB %= MOD;
+        int res = (int) ((xorA * xorB) % MOD);
         return res;
     }
 }
