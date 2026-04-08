55ca651f75a29a625904a78431febf19ee3de4e5
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 16:28:59 +0530

    Time: 50 ms (55.48%) | Memory: 89.7 MB (45.89%) - LeetSync

--- 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.java
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.java
@@ -1,34 +1,35 @@
 class Solution {
-    public void update(int[] binary, int num, int val) {
-        for (int i = 0; i < 32; i++) {
-            if (((num >> i) & 1) > 0) {
-                binary[i] += val;
+    private void addNumToBits(int[] bits, int num, int val) {
+        for (int i = 31; i >= 0; i--) {
+            if (((num >> i) & 1) >= 1) {
+                bits[i] += val;
             }
         }
     }
 
-    public int getDecimalFromBinary(int[] binary) {
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            if (binary[i] > 0) {
-                res |= (1 << i);
+    private int getNumFromBits(int[] bits) {
+        int num = 0;
+        for (int i = 31; i >= 0; i--) {
+            if (bits[i] > 0) {
+                num |= (1 << i);
             }
         }
-        return res;
+        return num;
     }
 
     public int minimumSubarrayLength(int[] nums, int k) {
-        int res = Integer.MAX_VALUE;
-        int s = 0, e = 0, n = nums.length;
-        int[] binary = new int[32];
-        while (e < n) {
-            update(binary, nums[e], 1);
-            while (s <= e && getDecimalFromBinary(binary) >= k) {
-                res = Math.min(res, e - s + 1);
-                update(binary, nums[s++], -1);
+        int[] bits = new int[32];
+        int minLen = Integer.MAX_VALUE;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            int num = nums[j];
+            addNumToBits(bits, num, 1);
+            while (getNumFromBits(bits) >= k && i <= j) {
+                minLen = Math.min(minLen, j - i + 1);
+                num = nums[i++];
+                addNumToBits(bits, num, -1);
             }
-            e++;
         }
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return minLen == Integer.MAX_VALUE ? -1 : minLen;
     }
 }
