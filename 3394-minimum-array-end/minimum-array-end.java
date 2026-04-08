33d4648146c7b4d66392684c69814c65504204f0
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 17:49:48 +0530

    Time: 1 ms (99.51%) | Memory: 42.4 MB (84.31%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.java
+++ 3394-minimum-array-end/minimum-array-end.java
@@ -1,9 +1,29 @@
 class Solution {
-    public long minEnd(int n, int x) {
+    private long sol1(int n, int x) {
         long num = x;
         for (int i = 0; i < n - 1; i++) {
             num = (num + 1) | x;
         }
         return num;
+    }
+
+    private long sol2(int n, int x) {
+        long res = x;
+        long rem = n - 1;
+        long pos = 0;
+        while (rem > 0) {
+            if (((res >> pos) & 1) == 0) {
+                if ((rem & 1) >= 1) {
+                    res |= (1L << pos);
+                }
+                rem = rem >> 1;
+            }
+            pos++;
+        }
+        return res;
+    }
+
+    public long minEnd(int n, int x) {
+        return sol2(n, x);
     }
 }
