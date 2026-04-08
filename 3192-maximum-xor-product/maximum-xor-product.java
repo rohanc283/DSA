8a9994550b3a8cbf6a00d73aec7e1f2e0ce188df
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 13:43:21 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (81.17%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.java
+++ 3192-maximum-xor-product/maximum-xor-product.java
@@ -1,35 +1,41 @@
 class Solution {
     public int maximumXorProduct(long a, long b, int n) {
-        long xorA = 0, xorB = 0;
-        for (long i = 49; i >= n; i--) {
-            boolean a_ith_bit = ((a >> i) & 1) >= 1;
-            boolean b_ith_bit = ((b >> i) & 1) >= 1;
-            if (a_ith_bit) {
-                xorA |= (1L << i);
-            }
-            if (b_ith_bit) {
-                xorB |= (1L << i);
-            }
-        }
+        long xor = 0;
 
         for (int i = n - 1; i >= 0; i--) {
-            boolean a_ith_bit = ((a >> i) & 1) >= 1;
-            boolean b_ith_bit = ((b >> i) & 1) >= 1;
-            if (a_ith_bit == b_ith_bit) {
-                xorA |= (1L << i);
-                xorB |= (1L << i);
+            int aBit = (int) ((a >> i) & 1);
+            int bBit = (int) ((b >> i) & 1);
+
+            if (aBit == bBit) {
+                if (aBit == 0) {
+                    xor |= (1L << i);
+                }
             } else {
-                if (xorA > xorB) {
-                    xorB |= (1L << i);
+                // 🔥 USE CURRENT STATE
+                long xor1 = xor ^ (1L << i); // flip
+                long xor2 = xor; // no flip
+
+                long A1 = a ^ xor1;
+                long B1 = b ^ xor1;
+
+                long A2 = a ^ xor2;
+                long B2 = b ^ xor2;
+
+                long diff1 = Math.abs(A1 - B1);
+                long diff2 = Math.abs(A2 - B2);
+
+                if (diff1 < diff2) {
+                    xor = xor1;
                 } else {
-                    xorA |= (1L << i);
+                    xor = xor2;
                 }
             }
         }
+
         int MOD = 1_000_000_007;
-        xorA %= MOD;
-        xorB %= MOD;
-        int res = (int) ((xorA * xorB) % MOD);
-        return res;
+        long an = (a ^ xor) % MOD;
+        long bn = (b ^ xor) % MOD;
+
+        return (int) ((an * bn) % MOD);
     }
 }
