5ca18b4648c7131cfbb381da3f5ad7b35d4d6103
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 14:23:47 +0530

    Time: 2 ms (100.00%) | Memory: 199 MB (65.85%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,29 +1,8 @@
 class Solution {
-    public boolean checkXor(int[] derived, int start) {
-        int n = derived.length;
-        int prev = start;
-
-        for (int i = 0; i < n - 1; i++) {
-            int curr = derived[i] ^ prev;
-            prev = curr;
-        }
-
-        return (prev ^ start) == derived[n - 1];
-    }
-
-    public boolean sol1(int[] derived) {
-        return checkXor(derived, 0) || checkXor(derived, 1);
-    }
-
-    public boolean sol2(int[] derived) {
-        int xor = derived[0];
-        for (int i = 1; i < derived.length; i++) {
-            xor ^= derived[i];
-        }
-        return xor == 0;
-    }
-
     public boolean doesValidArrayExist(int[] derived) {
-        return sol2(derived);
+        int xor = 0;
+        for (int d : derived)
+            xor ^= d;
+        return xor == 0;
     }
 }
