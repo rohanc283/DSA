f7eed914b8120846d67b1a70b2104efe65552dd6
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-08 18:28:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (82.75%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,16 +1,14 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
         int flips = 0;
-
-        for (int i = 0; i < 32; i++) {
-            int aBit = (a >> i) & 1;
-            int bBit = (b >> i) & 1;
-            int cBit = (c >> i) & 1;
-
-            if (cBit == 0) {
-                flips += aBit + bBit;
-            } else {
-                if ((aBit | bBit) == 0) {
+        for (int i = 31; i >= 0; i--) {
+            int aBit = (int) (a >> i) & 1;
+            int bBit = (int) (b >> i) & 1;
+            int cBit = (int) (c >> i) & 1;
+            if ((aBit | bBit) != cBit) {
+                if (cBit == 0) {
+                    flips += aBit + bBit;
+                } else {
                     flips++;
                 }
             }
