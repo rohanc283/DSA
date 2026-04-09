a830fd3b00086499ca6745d66394b8b719c31b87
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-09 21:30:06 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (53.74%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.java
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -1,36 +1,43 @@
 class Solution {
-    public int matrixScore(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        for (int i = 0; i < m; i++) {
-            if (grid[i][0] == 0) {
-                for (int j = 0; j < n; j++) {
-                    grid[i][j] = grid[i][j] == 0 ? 1 : 0;
-                }
-            }
+    private void toggleRow(int[][] grid, int r) {
+        for (int c = 0; c < grid[0].length; c++) {
+            grid[r][c] ^= 1;
         }
-        for (int j = 1; j < n; j++) {
-            int zeros = 0, ones = 0;
-            for (int i = 0; i < m; i++) {
-                if (grid[i][j] == 0)
-                    zeros++;
-                else
-                    ones++;
-            }
-            if (zeros > ones) {
-                for (int i = 0; i < m; i++) {
-                    grid[i][j] = grid[i][j] == 0 ? 1 : 0;
-                }
-            }
-        }
+    }
 
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            int curr = 0;
-            for (int j = 0; j < n; j++) {
-                curr = curr * 2 + grid[i][j];
-            }
-            res += curr;
+    private void toggleCol(int[][] grid, int c) {
+        for (int r = 0; r < grid.length; r++) {
+            grid[r][c] ^= 1;
         }
-        return res;
+    }
+
+    public int matrixScore(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            if (grid[i][0] == 0) {
+                toggleRow(grid, i);
+            }
+        }
+        for (int c = 1; c < m; c++) {
+            int one = 0, zero = 0;
+            for (int r = 0; r < n; r++) {
+                if (grid[r][c] == 0)
+                    zero++;
+                else
+                    one++;
+            }
+            if (zero > one) {
+                toggleCol(grid, c);
+            }
+        }
+        int sum = 0;
+        for (int r = 0; r < n; r++) {
+            int curr = 0;
+            for (int c = 0; c < m; c++) {
+                curr = curr * 2 + grid[r][c];
+            }
+            sum += curr;
+        }
+        return sum;
     }
 }
