9da05386b10ec7f7de7ed308e1b95755368a8df8
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-10 20:28:43 +0530

    Time: 10 ms (95.57%) | Memory: 90.9 MB (62.58%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,38 +1,36 @@
 class Solution {
     public int maximumGap(int[] nums) {
         int n = nums.length;
-        if (n < 2)
+        if (n == 1)
             return 0;
-
-        int maxi = 0, mini = Integer.MAX_VALUE;
+        int mini = nums[0], maxi = nums[0];
         for (int num : nums) {
-            maxi = Math.max(maxi, num);
             mini = Math.min(mini, num);
+            maxi = Math.max(maxi, num);
         }
 
-        int bucketSize = (int) Math.ceil((double) (maxi - mini) / (n - 1));
-        if (bucketSize == 0)
-            bucketSize = 1;
-        int[] minBucket = new int[n - 1];
-        Arrays.fill(minBucket, Integer.MAX_VALUE);
-        int[] maxBucket = new int[n - 1];
-        Arrays.fill(maxBucket, Integer.MIN_VALUE);
+        int bucketSize = ((maxi - mini) + (n - 1) - 1) / (n - 1);
+        int bucketLength = n - 1;
+        int[] bucketMin = new int[bucketLength];
+        Arrays.fill(bucketMin, Integer.MAX_VALUE);
+        int[] bucketMax = new int[bucketLength];
+        Arrays.fill(bucketMax, Integer.MIN_VALUE);
         for (int num : nums) {
-            if (num == mini || num == maxi)
+            if (num == maxi || num == mini)
                 continue;
             int bucketIdx = (num - mini) / bucketSize;
-            minBucket[bucketIdx] = Math.min(minBucket[bucketIdx], num);
-            maxBucket[bucketIdx] = Math.max(maxBucket[bucketIdx], num);
+            bucketMin[bucketIdx] = Math.min(bucketMin[bucketIdx], num);
+            bucketMax[bucketIdx] = Math.max(bucketMax[bucketIdx], num);
         }
-        int prev = mini;
         int res = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (minBucket[i] == Integer.MAX_VALUE)
+        int prevMax = mini;
+        for (int i = 0; i < bucketLength; i++) {
+            if (bucketMin[i] == Integer.MAX_VALUE)
                 continue;
-            res = Math.max(res, minBucket[i] - prev);
-            prev = maxBucket[i];
+            res = Math.max(res, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
         }
-        res = Math.max(maxi - prev, res);
+        res = Math.max(res, maxi - prevMax);
         return res;
     }
 }
