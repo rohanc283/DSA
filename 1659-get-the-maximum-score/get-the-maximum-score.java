9961a1fcdf7c80b04ffefcf45bddc7d5c56fa045
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-14 17:09:15 +0530

    Time: 5 ms (47.19%) | Memory: 61.6 MB (35.11%) - LeetSync

--- 1659-get-the-maximum-score/get-the-maximum-score.java
+++ 1659-get-the-maximum-score/get-the-maximum-score.java
@@ -1,33 +1,28 @@
 class Solution {
     public int maxSum(int[] nums1, int[] nums2) {
-
-        long sum1 = 0, sum2 = 0, result = 0;
-        int i = 0, j = 0;
+        long sum1 = 0, sum2 = 0;
+        int i = 0, j = 0, n1 = nums1.length, n2 = nums2.length;
         int MOD = 1_000_000_007;
 
-        while (i < nums1.length && j < nums2.length) {
+        while (i < n1 || j < n2) {
+            int num1 = i >= n1 ? Integer.MAX_VALUE : nums1[i];
+            int num2 = j >= n2 ? Integer.MAX_VALUE : nums2[j];
 
-            if (nums1[i] < nums2[j]) {
-                sum1 += nums1[i++];
-            } else if (nums1[i] > nums2[j]) {
-                sum2 += nums2[j++];
+            if (num1 < num2) {
+                sum1 += num1;
+                i++;
+            } else if (num1 > num2) {
+                sum2 += num2;
+                j++;
             } else {
-                result += Math.max(sum1, sum2) + nums1[i];
-                sum1 = 0;
-                sum2 = 0;
+                long max = Math.max(sum1, sum2) + num1;
+                sum1 = max;
+                sum2 = max;
                 i++;
                 j++;
             }
         }
 
-        while (i < nums1.length)
-            sum1 += nums1[i++];
-
-        while (j < nums2.length)
-            sum2 += nums2[j++];
-
-        result += Math.max(sum1, sum2);
-
-        return (int) (result % MOD);
+        return (int) (Math.max(sum1, sum2) % MOD);
     }
 }
