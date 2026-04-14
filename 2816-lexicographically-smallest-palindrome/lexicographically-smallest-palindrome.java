419cd0e2d502a0b0b27f5cefcce45ffc9c3bbf6a
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-14 18:30:38 +0530

    Time: 9 ms (20.93%) | Memory: 47 MB (65.37%) - LeetSync

--- 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
+++ 2816-lexicographically-smallest-palindrome/lexicographically-smallest-palindrome.java
@@ -1,13 +1,19 @@
 class Solution {
     public String makeSmallestPalindrome(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int l = 0, r = sb.length() - 1;
-
-        while (l < r) {
-            char c = (char) Math.min(sb.charAt(l), sb.charAt(r));
-            sb.setCharAt(l++, c);
-            sb.setCharAt(r--, c);
+        int i = 0, j = s.length() - 1;
+        StringBuilder res = new StringBuilder(s);
+        while (i < j) {
+            char c1 = res.charAt(i), c2 = res.charAt(j);
+            if (c1 != c2) {
+                if (c1 < c2) {
+                    res.setCharAt(j, c1);
+                } else {
+                    res.setCharAt(i, c2);
+                }
+            }
+            i++;
+            j--;
         }
-        return sb.toString();
+        return res.toString();
     }
 }
