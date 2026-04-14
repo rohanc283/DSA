2c66c23df157ed3f024ec3ce9d607f1a03cbe8b2
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-14 11:46:58 +0530

    Time: 388 ms (26.56%) | Memory: 71.7 MB (78.42%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -1,21 +1,22 @@
 class Solution {
     private int getMappedValue(int num, int[] mapping) {
         if (num == 0)
-            return mapping[num];
-        int mapped = 0;
-        int p = 1;
-        while (num > 0) {
-            int digit = num % 10;
-            mapped = mapping[digit] * p + mapped;
-            num /= 10;
-            p *= 10;
+            return mapping[0];
+        int res = 0;
+        int d = 1;
+        while (num / d > 0) {
+            int digit = (num / d) % 10;
+            int mappedDigit = mapping[digit];
+            res = mappedDigit * d + res;
+            d *= 10;
         }
-        return mapped;
+        return res;
     }
 
     public int[] sortJumbled(int[] mapping, int[] nums) {
         return Arrays.stream(nums).boxed()
-                .sorted((a, b) -> getMappedValue(a, mapping) - getMappedValue(b, mapping))
-                .mapToInt(Integer::intValue).toArray();
+                .sorted((a, b) -> Integer.compare(getMappedValue(a, mapping), getMappedValue(b, mapping)))
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
