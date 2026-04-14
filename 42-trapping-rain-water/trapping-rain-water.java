16e4338c0f4921a710df7e3c25eb07a76e5d75bb
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-14 16:23:27 +0530

    Time: 1 ms (58.76%) | Memory: 47.6 MB (84.67%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,76 +1,23 @@
 class Solution {
-    public int sol1(int[] height) {
-        int n = height.length;
-        int maxi = Integer.MIN_VALUE, leftMaxiIdx = -1;
-        for (int i = 0; i < n; i++) {
-            if (height[i] > maxi) {
-                maxi = height[i];
-                leftMaxiIdx = i;
-            }
-        }
-        int leftMax = 0;
-        int res = 0;
-        for (int i = 0; i <= leftMaxiIdx; i++) {
-            if (leftMax > height[i]) {
-                res += (leftMax - height[i]);
-            }
-            leftMax = Math.max(leftMax, height[i]);
-        }
-
-        maxi = Integer.MIN_VALUE;
-        int rightMaxiIdx = -1;
-        for (int i = 0; i < n; i++) {
-            if (height[i] > maxi) {
-                maxi = height[i];
-                rightMaxiIdx = i;
-            }
-        }
-        int rightMax = 0;
-        for (int i = n - 1; i >= rightMaxiIdx; i--) {
-            if (rightMax > height[i]) {
-                res += (rightMax - height[i]);
-            }
-            rightMax = Math.max(rightMax, height[i]);
-        }
-        return res;
-    }
-
-    public int sol2(int[] height) {
-        int n = height.length;
-        Deque<Integer> stack = new ArrayDeque<>();
-        int res = 0;
-        for (int i = 0; i < height.length; i++) {
-            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
-                int bottom = stack.pop();
-                if (stack.isEmpty())
-                    break;
-                int left = stack.peek();
-                int width = i - left - 1;
-                int boundary = Math.min(height[i], height[left]) - height[bottom];
-                res += width * boundary;
-            }
-            stack.push(i);
-        }
-        return res;
-    }
-
-    public int sol3(int[] height) {
-        int n = height.length;
-        int i = 0, j = n - 1, leftMax = 0, rightMax = 0;
-        int res = 0;
+    public int trap(int[] height) {
+        int leftMax = 0, rightMax = 0;
+        int i = 0, j = height.length - 1;
+        int water = 0;
         while (i < j) {
             leftMax = Math.max(leftMax, height[i]);
             rightMax = Math.max(rightMax, height[j]);
-            if (leftMax < rightMax) {
-                res += leftMax - height[i++];
-            } else {
-                res += rightMax - height[j--];
+            int level = Math.min(leftMax, rightMax);
+            if (level - height[i] >= 0) {
+                water += level - height[i];
             }
+            if (level - height[j] >= 0) {
+                water += level - height[j];
+            }
+            if (leftMax <= rightMax)
+                i++;
+            else if (rightMax < leftMax)
+                j--;
         }
-        return res;
-    }
-
-    public int trap(int[] height) {
-        return sol3(height);
+        return water;
     }
 }
