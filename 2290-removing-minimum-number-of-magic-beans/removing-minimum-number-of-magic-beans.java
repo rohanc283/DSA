2763fca7311b3fa782cab13f43b89ae2a1a64341
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-15 17:47:29 +0530

    Time: 41 ms (19.56%) | Memory: 108.2 MB (32.89%) - LeetSync

--- 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.java
+++ 2290-removing-minimum-number-of-magic-beans/removing-minimum-number-of-magic-beans.java
@@ -1,18 +1,19 @@
 class Solution {
     public long minimumRemoval(int[] beans) {
-        Arrays.sort(beans);
         int n = beans.length;
-        long totalSum = 0;
+        Arrays.sort(beans);
+        long total = 0;
+        for (int bean : beans)
+            total += 1L * bean;
+        long leftBeans = 0;
+        long minRemoval = Long.MAX_VALUE;
         for (int i = 0; i < n; i++) {
-            totalSum += beans[i];
+            int rem = n - i - 1;
+            long left = total - (leftBeans + beans[i]);
+            long right = Math.abs((1L * beans[i] * rem) - left);
+            minRemoval = Math.min(minRemoval, leftBeans + right);
+            leftBeans += beans[i];
         }
-        long ans = Long.MAX_VALUE;
-        long leftSum = 0;
-        for (int i = 0; i < n; i++) {
-            long rightSum = (totalSum - (leftSum + beans[i])) - (beans[i] * 1L * (n - i - 1));
-            ans = Math.min(ans, leftSum + rightSum);
-            leftSum += beans[i];
-        }
-        return ans;
+        return minRemoval;
     }
 }
