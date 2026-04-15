2be1ab62e301f09430834eff86dacb2f79818ae4
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-15 13:06:07 +0530

    Time: 14 ms (44.79%) | Memory: 73.9 MB (52.08%) - LeetSync

--- 2787-movement-of-robots/movement-of-robots.java
+++ 2787-movement-of-robots/movement-of-robots.java
@@ -1,30 +1,26 @@
 class Solution {
     public int sumDistance(int[] nums, String s, int d) {
+        int MOD = 1_000_000_007;
         int n = nums.length;
-        int mod = 1_000_000_007;
-
         long[] pos = new long[n];
-
         for (int i = 0; i < n; i++) {
             if (s.charAt(i) == 'R') {
-                pos[i] = (long) nums[i] + d;
+                pos[i] = nums[i] + (long) d;
             } else {
-                pos[i] = (long) nums[i] - d;
+                pos[i] = nums[i] - (long) d;
             }
         }
 
         Arrays.sort(pos);
+        long res = 0;
+        long sum = pos[0];
 
-        long ans = 0;
-        long leftSum = 0;
-
-        for (int i = 0; i < n; i++) {
-            ans = (ans + (long) i * pos[i] - leftSum) % mod;
-            leftSum = (leftSum + pos[i]) % mod;
+        for (int i = 1; i < n; i++) {
+            long curr = ((long) pos[i] * i % MOD - sum % MOD + MOD) % MOD;
+            res = (res + curr) % MOD;
+            sum = (sum + pos[i]) % MOD;
         }
 
-        if (ans < 0)
-            ans += mod;
-        return (int) ans;
+        return (int) res;
     }
 }
