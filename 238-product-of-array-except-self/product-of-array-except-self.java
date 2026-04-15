85b707f62434918b702e48a51fbbf11d14753d26
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-15 16:08:52 +0530

    Time: 2 ms (95.13%) | Memory: 64.9 MB (82.76%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,45 +1,22 @@
 class Solution {
-    public int[] solveWithSpace(int[] nums) {
+    public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] left = new int[n];
         int[] right = new int[n];
-        int p = 1;
-        for (int i = 0; i < n; i++) {
-            left[i] = p;
-            p *= nums[i];
+        left[0] = nums[0];
+        right[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i];
         }
-
-        p = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            right[i] = p;
-            p *= nums[i];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i];
         }
-
         int[] res = new int[n];
         for (int i = 0; i < n; i++) {
-            res[i] = left[i] * right[i];
+            int l = i > 0 ? left[i - 1] : 1;
+            int r = i < n - 1 ? right[i + 1] : 1;
+            res[i] = l * r;
         }
         return res;
-    }
-
-    public int[] solveWithoutSpace(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        int p = 1;
-        for (int i = 0; i < n; i++) {
-            res[i] = p;
-            p *= nums[i];
-        }
-
-        p = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            res[i] *= p;
-            p *= nums[i];
-        }
-        return res;
-    }
-
-    public int[] productExceptSelf(int[] nums) {
-        return solveWithoutSpace(nums);
     }
 }
