970c219adfcff861a034fdd13579d4dc7a4d8448
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-16 13:04:15 +0530

    Time: 19 ms (22.82%) | Memory: 63.9 MB (18.93%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
@@ -1,63 +1,32 @@
 class Solution {
-    public long findCost(int[] nums, int[] cost, long x) {
-        long total = 0;
-        for (int i = 0; i < nums.length; i++) {
-            total += Math.abs(nums[i] - x) * cost[i];
-        }
-        return total;
-    }
-
-    public long ternarySearch(int[] nums, int[] cost) {
-        long l = Long.MAX_VALUE, r = Long.MIN_VALUE;
-        for (int num : nums) {
-            l = Math.min(l, num);
-            r = Math.max(r, num);
-        }
-
-        long ans = Long.MAX_VALUE;
-        while (l <= r) {
-            long mid = l + (r - l) / 2;
-            long c1 = findCost(nums, cost, mid);
-            long c2 = findCost(nums, cost, mid + 1);
-
-            ans = Math.min(ans, Math.min(c1, c2));
-            if (c1 < c2)
-                r = mid - 1;
-            else
-                l = mid + 1;
-        }
-        return ans;
-    }
-
-    public long prefix(int[] nums, int[] cost) {
+    public long minCost(int[] nums, int[] cost) {
         int n = nums.length;
-
-        Integer[][] numsWithCost = new Integer[n][2];
-        long totalCost = 0;
-
+        int[][] numsWithCost = new int[n][2];
+        long total = 0;
         for (int i = 0; i < n; i++) {
             numsWithCost[i][0] = nums[i];
             numsWithCost[i][1] = cost[i];
-            totalCost += cost[i];
+            total += 1L * cost[i];
         }
+        Arrays.sort(numsWithCost, (a, b) -> Long.compare(a[0], b[0]));
 
-        Arrays.sort(numsWithCost, (a, b) -> Integer.compare(a[0], b[0]));
-
-        long prefix = 0;
+        // find median;
+        long curr = 0;
         long median = 0;
-
         for (int i = 0; i < n; i++) {
-            prefix += numsWithCost[i][1];
-            if (prefix * 2 >= totalCost) {
-                median = numsWithCost[i][0];
+            curr += numsWithCost[i][1];
+            if (curr > total / 2) {
+                median = 1L * numsWithCost[i][0];
                 break;
             }
         }
 
-        return findCost(nums, cost, median);
-    }
-
-    public long minCost(int[] nums, int[] cost) {
-        return ternarySearch(nums, cost);
+        // find cost
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            long d = Math.abs(numsWithCost[i][0] - median);
+            res += (d * 1L * numsWithCost[i][1]);
+        }
+        return res;
     }
 }
