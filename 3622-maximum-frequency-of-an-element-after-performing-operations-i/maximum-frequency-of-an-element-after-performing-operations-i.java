e9c762116571fe5e7ed0a2e39e5dc2fb5aaf73ec
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-16 12:41:28 +0530

    Time: 1272 ms (5.12%) | Memory: 77.4 MB (100.00%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,65 +1,32 @@
 class Solution {
-    public int prefixSumTech(int[] nums, int k, int numOperations) {
-        int maxEl = Integer.MIN_VALUE;
+    public int maxFrequency(int[] nums, int k, int numOperations) {
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            maxEl = Math.max(maxEl, nums[i]);
-        }
-        int[] freq = new int[maxEl + 1];
-        for (int i = 0; i < n; i++)
-            freq[nums[i]]++;
+        int maxi = 0;
 
-        for (int i = 1; i <= maxEl; i++)
+        // find maximum
+        for (int num : nums)
+            maxi = Math.max(maxi, num);
+
+        // storing freq
+        int[] freq = new int[maxi + 1];
+        for (int num : nums)
+            freq[num]++;
+
+        // cumulative sum
+        for (int i = 1; i <= maxi; i++) {
             freq[i] += freq[i - 1];
+        }
 
-        int res = 0;
-        for (int target = 0; target <= maxEl; target++) {
+        int maxFreq = 0;
+        for (int target = 0; target <= maxi; target++) {
             if (freq[target] == 0)
                 continue;
-            int leftNum = Math.max(0, target - k);
-            int rightNum = Math.min(maxEl, target + k);
-            int totalCount = freq[rightNum] - (leftNum > 0 ? freq[leftNum - 1] : 0);
-            int targetCount = freq[target] - (target > 0 ? freq[target - 1] : 0);
-            int needConversion = totalCount - targetCount;
-            int maxPosFreq = targetCount + Math.min(numOperations, needConversion);
-            res = Math.max(res, maxPosFreq);
+            int totalFreq = freq[Math.min(target + k, maxi)] - (target - k > 0 ? freq[target - k - 1] : 0);
+            System.out.println(totalFreq);
+            int currNumFreq = freq[target] - (target > 0 ? freq[target - 1] : 0);
+            int reqOp = totalFreq - currNumFreq;
+            maxFreq = Math.max(maxFreq, currNumFreq + Math.min(reqOp, numOperations));
         }
-        return res;
-    }
-
-    public int diffArrayTech(int[] nums, int k, int numOperations) {
-        int maxEl = Integer.MIN_VALUE;
-        int n = nums.length;
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            maxEl = Math.max(maxEl, nums[i]);
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
-        }
-
-        int[] diff = new int[maxEl + 1];
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int left = Math.max(0, num - k);
-            int right = Math.min(maxEl, num + k);
-            diff[left]++;
-            if (right + 1 <= maxEl) {
-                diff[right + 1]--;
-            }
-        }
-
-        int res = 0;
-        for (int target = 0; target <= maxEl; target++) {
-            diff[target] += (target > 0 ? diff[target - 1] : 0);
-            int targetCount = freq.getOrDefault(target, 0);
-            int needConversion = diff[target] - targetCount;
-            int maxPosFreq = targetCount + Math.min(numOperations, needConversion);
-            res = Math.max(res, maxPosFreq);
-        }
-        return res;
-    }
-
-    public int maxFrequency(int[] nums, int k, int numOperations) {
-        // return prefixSumTech(nums, k, numOperations);
-        return diffArrayTech(nums, k, numOperations);
+        return maxFreq;
     }
 }
