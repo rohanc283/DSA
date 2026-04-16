35b8fc43e9c8ddbf734055687a3df69c2939d561
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-16 13:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (65.03%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,55 +1,20 @@
 class Solution {
-    public int lowerBound(int[] nums, int target) {
-        int s = 0, e = nums.length - 1;
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (nums[m] >= target) {
-                e = m - 1;
-            } else {
-                s = m + 1;
-            }
-        }
-        return s;
-    }
-
-    public int sol1(int[] nums) {
-        Arrays.sort(nums);
+    public int specialArray(int[] nums) {
         int n = nums.length;
-        int s = 0, e = n;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            int count = n - lowerBound(nums, mid);
-            if (count == mid) {
-                return mid;
-            } else if (count > mid) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    public int sol2(int[] nums) {
-        int n = nums.length;
-        int[] arr = new int[n + 1];
+        int[] freq = new int[n + 1];
         for (int num : nums) {
-            if (num >= n) {
-                arr[n]++;
-            } else {
-                arr[num]++;
+            if (num >= n)
+                freq[n]++;
+            else {
+                freq[num]++;
             }
         }
-        int sum = 0;
+        int suffix = 0;
         for (int i = n; i >= 0; i--) {
-            sum += arr[i];
-            if (sum == i)
+            suffix += freq[i];
+            if (suffix == i)
                 return i;
         }
         return -1;
-    }
-
-    public int specialArray(int[] nums) {
-        return sol1(nums);
     }
 }
