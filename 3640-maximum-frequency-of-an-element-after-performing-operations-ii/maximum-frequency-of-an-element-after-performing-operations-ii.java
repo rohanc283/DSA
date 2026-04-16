81c9075fd9e69e65157dbf51f60681e20af767c9
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-16 19:46:08 +0530

    Time: 1086 ms (13.47%) | Memory: 197.2 MB (33.06%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.java
@@ -1,37 +1,28 @@
 class Solution {
     public int maxFrequency(int[] nums, int k, int numOperations) {
         int n = nums.length;
-        Map<Integer, Integer> diff = new TreeMap<>();
         Map<Integer, Integer> freq = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int l = Math.max(0, num - k);
+        Map<Integer, Integer> diff = new TreeMap<>();
+        for (int num : nums) {
+            int l = Math.max(num - k, 0);
             int r = num + k + 1;
+
             diff.put(l, diff.getOrDefault(l, 0) + 1);
             diff.put(r, diff.getOrDefault(r, 0) - 1);
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-
-        for (int i = 0; i < n; i++) {
-            if (diff.containsKey(nums[i])) {
+        for (int num : nums) {
+            if (diff.containsKey(num))
                 continue;
-            }
-            diff.put(nums[i], 0);
+            diff.put(num, 0);
         }
-
-        int res = 0;
-        int sum = 0;
-        for (Map.Entry<Integer, Integer> entry : diff.entrySet()) {
-            int target = entry.getKey();
-            sum += entry.getValue();
-            diff.put(target, sum);
-
+        int maxFreq = 0;
+        int prefix = 0;
+        for (Integer target : diff.keySet()) {
+            prefix += diff.get(target);
             int targetCount = freq.getOrDefault(target, 0);
-            int needConversion = sum - targetCount;
-            int maxPosFreq = targetCount + Math.min(numOperations, needConversion);
-            res = Math.max(res, maxPosFreq);
+            maxFreq = Math.max(maxFreq, targetCount + Math.min(prefix - targetCount, numOperations));
         }
-        return res;
+        return maxFreq;
     }
 }
