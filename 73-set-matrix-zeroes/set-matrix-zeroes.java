a5ebf335944a3e895a6563118f77bdbb05b66767
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-17 16:14:06 +0530

    Time: 1 ms (85.19%) | Memory: 47.7 MB (32.92%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,48 +1,19 @@
 class Solution {
-    public void sol1(int[][] matrix) {
-        List<List<Integer>> zeros = new ArrayList<>();
+    public void setZeroes(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
+        boolean firstRowHasZero = false, firstColHasZero = false;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+            }
+        }
+
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    zeros.add(new ArrayList<>(List.of(i, j)));
-                }
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
             }
         }
 
-        for (int i = 0; i < zeros.size(); i++) {
-            int r = zeros.get(i).get(0);
-            int c = zeros.get(i).get(1);
-            for (int j = 0; j < n; j++) {
-                matrix[r][j] = 0;
-            }
-            for (int j = 0; j < m; j++) {
-                matrix[j][c] = 0;
-            }
-        }
-    }
-
-    public void sol2(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        boolean firstRowZero = false, firstColZero = false;
-
-        // Check first row
-        for (int c = 0; c < n; c++) {
-            if (matrix[0][c] == 0) {
-                firstRowZero = true;
-                break;
-            }
-        }
-
-        // Check first column
-        for (int r = 0; r < m; r++) {
-            if (matrix[r][0] == 0) {
-                firstColZero = true;
-                break;
-            }
-        }
-
-        // Mark rows & cols
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
@@ -52,7 +23,6 @@ class Solution {
             }
         }
 
-        // Zero based on markers
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -61,20 +31,15 @@ class Solution {
             }
         }
 
-        // Restore first row
-        if (firstRowZero) {
-            for (int c = 0; c < n; c++)
-                matrix[0][c] = 0;
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
         }
-
-        // Restore first column
-        if (firstColZero) {
-            for (int r = 0; r < m; r++)
-                matrix[r][0] = 0;
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
         }
-    }
-
-    public void setZeroes(int[][] matrix) {
-        sol2(matrix);
     }
 }
