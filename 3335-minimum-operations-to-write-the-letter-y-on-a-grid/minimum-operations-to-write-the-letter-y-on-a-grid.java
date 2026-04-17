74e8851a80186d48beeaebfab1e7297cf70b38ef
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-17 17:50:36 +0530

    Time: 3 ms (59.73%) | Memory: 46.8 MB (86.58%) - LeetSync

--- 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.java
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/minimum-operations-to-write-the-letter-y-on-a-grid.java
@@ -1,73 +1,34 @@
 class Solution {
-    public int findCost(int[][] grid, int yVal, int oVal) {
-        int n = grid.length, centre = n / 2;
-        int cost = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == j && i < centre && j < centre)
-                        || (i + j == n - 1 && i < centre && j > centre)
-                        || (i >= centre && j == centre)) {
-                    if (grid[i][j] != yVal)
-                        cost++;
-                } else {
-                    if (grid[i][j] != oVal)
-                        cost++;
-                }
-            }
-        }
-        return cost;
-    }
-
-    public int freqSol(int[][] grid) {
-        int n = grid.length, centre = n / 2;
-
-        int[] freqY = new int[3];
-        int[] freqNonY = new int[3];
-        int totalY = 0, totalNonY = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                boolean isY = (i == j && i < centre) ||
-                        (i + j == n - 1 && i < centre) ||
-                        (i >= centre && j == centre);
-
-                if (isY) {
-                    freqY[grid[i][j]]++;
-                    totalY++;
-                } else {
-                    freqNonY[grid[i][j]]++;
-                    totalNonY++;
-                }
-            }
-        }
-
-        int ans = Integer.MAX_VALUE;
-
-        for (int yVal = 0; yVal <= 2; yVal++) {
-            for (int oVal = 0; oVal <= 2; oVal++) {
-                if (yVal == oVal)
-                    continue;
-                int cost = (totalY - freqY[yVal]) + (totalNonY - freqNonY[oVal]);
-                ans = Math.min(ans, cost);
-            }
-        }
-
-        return ans;
+    private boolean isY(int i, int j, int n, int centre) {
+        return (j <= centre && i == j) || (i < centre && j == (n - i - 1)) || (i > centre && j == centre);
     }
 
     public int minimumOperationsToWriteY(int[][] grid) {
-        // int ans = Integer.MAX_VALUE;
-        // int[] values = { 0, 1, 2 };
-
-        // for (int yVal : values) {
-        //     for (int oVal : values) {
-        //         if (yVal == oVal)
-        //             continue;
-        //         ans = Math.min(ans, findCost(grid, yVal, oVal));
-        //     }
-        // }
-
-        // return ans;
-        return freqSol(grid);
+        int n = grid.length;
+        int centre = n / 2;
+        int[] freqY = new int[3];
+        int[] freqNonY = new int[3];
+        int totalY = 0, totalNonY = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = grid[i][j];
+                if (isY(i, j, n, centre)) {
+                    totalY++;
+                    freqY[num]++;
+                } else {
+                    totalNonY++;
+                    freqNonY[num]++;
+                }
+            }
+        }
+        int minOp = Integer.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j)
+                    continue;
+                minOp = Math.min(minOp, (totalY - freqY[i]) + (totalNonY - freqNonY[j]));
+            }
+        }
+        return minOp;
     }
 }
