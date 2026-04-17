743a1bdead20ca58c6a429b8dcaa131b5e40e6a9
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-17 18:14:30 +0530

    Time: 14 ms (6.81%) | Memory: 47.3 MB (32.98%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,60 +1,31 @@
 class Solution {
-    private List<List<Integer>> toList(int[][] grid) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int[] row : grid) {
-            List<Integer> listRow = new ArrayList<>();
-            for (int val : row)
-                listRow.add(val);
-            res.add(listRow);
-        }
-        return res;
-    }
-
-    public List<List<Integer>> inPlace(int[][] grid, int k) {
-        int m = grid.length, n = grid[0].length;
-        int total = m * n;
-        k = k % total;
-
-        if (k == 0)
-            return toList(grid);
-
-        int[] temp = new int[k];
-        for (int t = 0; t < k; t++) {
-            int idx = total - k + t;
-            temp[t] = grid[idx / n][idx % n];
-        }
-
-        for (int idx = total - k - 1; idx >= 0; idx--) {
-            int newIdx = idx + k;
-            grid[newIdx / n][newIdx % n] = grid[idx / n][idx % n];
-        }
-
-        for (int i = 0; i < k; i++) {
-            grid[i / n][i % n] = temp[i];
-        }
-
-        return toList(grid);
-    }
-
-    public List<List<Integer>> withSpace(int[][] grid, int k) {
-        int m = grid.length, n = grid[0].length;
-        int total = m * n;
-        k = k % total;
-
-        if (k == 0)
-            return toList(grid);
-        int[][] res = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int idx = (i * n) + j;
-                int newIdx = (idx + k) % total;
-                res[newIdx / n][newIdx % n] = grid[i][j];
-            }
-        }
-        return toList(res);
+    private void rotateByK(List<Integer> temp, int k) {
+        int n = temp.size();
+        k = k % n;
+        Collections.reverse(temp.subList(0, n - k));
+        Collections.reverse(temp.subList(n - k, n));
+        Collections.reverse(temp.subList(0, n));
     }
 
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
-        return withSpace(grid, k);
+        int m = grid.length, n = grid[0].length;
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                temp.add(grid[i][j]);
+            }
+        }
+        rotateByK(temp, k);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            res.add(Arrays.stream(grid[i]).boxed().collect(Collectors.toList()));
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int idx = i * n + j;
+                res.get(i).set(j, temp.get(idx));
+            }
+        }
+        return res;
     }
 }
