7c6a22a21a30a8844d090149d7b9bd68612810bb
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-17 16:21:53 +0530

    Time: 8 ms (67.13%) | Memory: 46.8 MB (56.50%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.java
@@ -1,92 +1,26 @@
 class Solution {
-    public int[][] sol1(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        List<List<Integer>> diagList = new ArrayList<>();
-        int r = m - 1, c = 0;
-        while (r >= 0 && c < n) {
-            List<Integer> diag = new ArrayList<>();
-            int i = r, j = c;
-            while (i < m && j < n) {
-                diag.add(mat[i][j]);
-                i++;
-                j++;
-            }
-            if (diag.size() > 0)
-                diagList.add(diag);
-            if (r - 1 < 0) {
-                c++;
-            } else {
-                r--;
-            }
-        }
-        for (List<Integer> diag : diagList) {
-            Collections.sort(diag);
-        }
-
-        r = m - 1;
-        c = 0;
-        int diagIndex = 0;
-        while (r >= 0 && c < n) {
-            int i = r, j = c, k = 0;
-            while (i < m && j < n) {
-                mat[i++][j++] = diagList.get(diagIndex).get(k++);
-            }
-            if (r - 1 < 0) {
-                c++;
-            } else {
-                r--;
-            }
-            diagIndex++;
-        }
-        return mat;
-    }
-
-    public int[][] sol2(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        Map<Integer, List<Integer>> diagMap = new TreeMap<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                diagMap.computeIfAbsent(i - j, k -> new ArrayList<>())
-                        .add(mat[i][j]);
-            }
-        }
-
-        for (List<Integer> list : diagMap.values()) {
-            Collections.sort(list);
-        }
-
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                List<Integer> list = diagMap.get(i - j);
-                mat[i][j] = list.remove(list.size() - 1);
-            }
-        }
-
-        return mat;
-    }
-
-    public int[][] sol3(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        Map<Integer, PriorityQueue<Integer>> diagMap = new HashMap<>();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                diagMap.computeIfAbsent(i - j, k -> new PriorityQueue<>())
-                        .offer(mat[i][j]);
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                mat[i][j] = diagMap.get(i - j).poll();
-            }
-        }
-
-        return mat;
-    }
-
     public int[][] diagonalSort(int[][] mat) {
-        return sol3(mat);
+        int m = mat.length, n = mat[0].length;
+        int r = 0, c = n - 1;
+        while (r < m || c >= 0) {
+            int i = r, j = c == -1 ? 0 : c;
+            List<Integer> nums = new ArrayList<>();
+            while (i < m && j < n) {
+                nums.add(mat[i++][j++]);
+            }
+            Collections.sort(nums);
+            i = r;
+            j = c == -1 ? 0 : c;
+            int idx = 0;
+            while (i < m && j < n) {
+                mat[i++][j++] = nums.get(idx++);
+            }
+            if (c < 0) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return mat;
     }
 }
