7c08bc534183a403499f25e9e422de871f6e7b45
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-21 12:56:34 +0530

    Time: 5 ms (94.63%) | Memory: 239.9 MB (31.71%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minimumArea(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int sr = m - 1, er = 0;
-        int sc = n - 1, ec = 0;
+        int sr = m, sc = n, er = -1, ec = -1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
                     sr = Math.min(sr, i);
-                    er = Math.max(er, i);
                     sc = Math.min(sc, j);
+
+                    er = Math.max(er, i);
                     ec = Math.max(ec, j);
                 }
             }
         }
-        return (er - sr + 1) * (ec - sc + 1);
+        return sr == m && sc == n ? 0 : (er - sr + 1) * (ec - sc + 1);
     }
 }
