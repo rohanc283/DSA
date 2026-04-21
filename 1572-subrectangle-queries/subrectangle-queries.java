8f16fa3a382b54fa1b28c6a0250a1f59e2cb7686
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-21 12:39:55 +0530

    Time: 24 ms (4.88%) | Memory: 48.8 MB (90.24%) - LeetSync

--- 1572-subrectangle-queries/subrectangle-queries.java
+++ 1572-subrectangle-queries/subrectangle-queries.java
@@ -1,21 +1,24 @@
 class SubrectangleQueries {
-
     int[][] matrix;
+    List<List<Integer>> updates;
 
     public SubrectangleQueries(int[][] rectangle) {
         matrix = rectangle;
+        updates = new ArrayList<>();
     }
 
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-        for (int i = row1; i <= row2; i++) {
-            for (int j = col1; j <= col2; j++) {
-                matrix[i][j] = newValue;
-            }
-        }
+        updates.add(Arrays.asList(row1, col1, row2, col2, newValue));
     }
 
     public int getValue(int row, int col) {
-        return matrix[row][col];
+        int value = matrix[row][col];
+        for (List<Integer> update : updates) {
+            if (row >= update.get(0) && row <= update.get(2) && col >= update.get(1) && col <= update.get(3)) {
+                value = update.get(4);
+            }
+        }
+        return value;
     }
 }
 
