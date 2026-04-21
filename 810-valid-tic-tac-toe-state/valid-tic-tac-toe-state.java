96a4ffb623f3cb27af75a0984d3a82bb29ef64e3
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-21 18:23:55 +0530

    Time: 6 ms (6.39%) | Memory: 42.5 MB (89.50%) - LeetSync

--- 810-valid-tic-tac-toe-state/valid-tic-tac-toe-state.java
+++ 810-valid-tic-tac-toe-state/valid-tic-tac-toe-state.java
@@ -1,60 +1,75 @@
 class Solution {
-    public boolean checkWon(String[] board, char ch) {
+    private boolean won(String[] board, char c) {
+        for (int i = 0; i < 3; i++) {
+            boolean isValid = true;
+            for (int j = 0; j < 3; j++) {
+                if (board[i].charAt(j) != c) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid)
+                return true;
+        }
 
         for (int i = 0; i < 3; i++) {
-            if (board[i].charAt(0) == ch &&
-                    board[i].charAt(1) == ch &&
-                    board[i].charAt(2) == ch)
+            boolean isValid = true;
+            for (int j = 0; j < 3; j++) {
+                if (board[j].charAt(i) != c) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid)
                 return true;
         }
 
-        for (int j = 0; j < 3; j++) {
-            if (board[0].charAt(j) == ch &&
-                    board[1].charAt(j) == ch &&
-                    board[2].charAt(j) == ch)
-                return true;
+        boolean isValid = true;
+        for (int i = 2; i >= 0; i--) {
+            if (board[i].charAt(i) != c) {
+                isValid = false;
+                break;
+            }
         }
 
-        if (board[0].charAt(0) == ch &&
-                board[1].charAt(1) == ch &&
-                board[2].charAt(2) == ch)
+        if (isValid)
             return true;
 
-        if (board[0].charAt(2) == ch &&
-                board[1].charAt(1) == ch &&
-                board[2].charAt(0) == ch)
-            return true;
-
-        return false;
+        isValid = true;
+        for (int i = 2, j = 0; i >= 0 && j < 3; i--, j++) {
+            if (board[i].charAt(j) != c) {
+                isValid = false;
+                break;
+            }
+        }
+        return isValid;
     }
 
     public boolean validTicTacToe(String[] board) {
         int xCount = 0, oCount = 0;
-
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[i].charAt(j) == 'X')
                     xCount++;
-                if (board[i].charAt(j) == 'O')
+                else if (board[i].charAt(j) == 'O')
                     oCount++;
             }
         }
-
-        if (oCount > xCount || xCount - oCount > 1)
+        System.out.println(xCount + " " + oCount);
+        if (oCount > xCount)
             return false;
-
-        boolean xWon = checkWon(board, 'X');
-        boolean oWon = checkWon(board, 'O');
-
-        if (xWon && oWon)
+        if (Math.abs(xCount - oCount) >= 2)
             return false;
-
-        if (xWon)
-            return xCount == oCount + 1;
-        if (oWon)
+        boolean xWon = won(board, 'X');
+        boolean oWon = won(board, 'O');
+        if(xWon && oWon) return false;
+        else if (xWon) {
+            return xCount - oCount == 1;
+        } else if (oWon) {
             return xCount == oCount;
-
-        return true;
+        } else {
+            System.out.println(xCount + " " + oCount);
+            return xCount >= oCount;
+        }
     }
-
 }
