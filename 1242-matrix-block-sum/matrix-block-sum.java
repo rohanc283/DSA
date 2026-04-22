276cddb274d44a81dc9c05628951a556def8f124
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-22 17:20:11 +0530

    Time: 8 ms (30.89%) | Memory: 46.8 MB (46.95%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -14,21 +14,12 @@ class Solution {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int r1 = i - k, r2 = i + k, c1 = j - k, c2 = j + k;
-                // System.out.println(r1 + " " + c1 + " " + r2 + " " + c2);
-
                 int p1 = prefix[Math.min(m - 1, r2)][Math.min(n - 1, c2)];
                 int p2 = c1 <= 0 ? 0 : prefix[Math.min(m - 1, r2)][Math.max(0, c1 - 1)];
-
                 int p3 = r1 <= 0 ? 0 : prefix[Math.max(0, r1 - 1)][Math.min(n - 1, c2)];
                 int p4 = r1 <= 0 || c1 <= 0 ? 0 : prefix[Math.max(0, r1 - 1)][Math.max(0, c1 - 1)];
-                // System.out.println(p1 + " " + p2 + " " + p3 + " " + p4);
                 mat[i][j] = p1 - p2 - p3 + p4;
-
-                // System.out.println(mat[i][j]);
-                // System.out.println();
-
             }
-            System.out.println();
         }
         return mat;
 
