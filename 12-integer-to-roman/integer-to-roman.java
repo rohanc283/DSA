7632b7371718c285de8211863289e6427906052b
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-23 15:25:05 +0530

    Time: 6 ms (15.62%) | Memory: 46.6 MB (28.44%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,17 +1,30 @@
 class Solution {
     public String intToRoman(int num) {
-        int[] value = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-        String[] sym = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < value.length; i++) {
-            if (num == 0)
-                break;
-            int times = num / value[i];
-            while (times > 0) {
-                res.append(sym[i]);
-                times--;
+        Map<Integer, String> map = new HashMap<>() {
+            {
+                put(1, "I");
+                put(4, "IV");
+                put(5, "V");
+                put(9, "IX");
+                put(10, "X");
+                put(40, "XL");
+                put(50, "L");
+                put(90, "XC");
+                put(100, "C");
+                put(400, "CD");
+                put(500, "D");
+                put(900, "CM");
+                put(1000, "M");
             }
-            num %= value[i];
+        };
+        StringBuilder res = new StringBuilder();
+        int[] divisor = new int[] { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        int divIdx = 0;
+        while (num > 0) {
+            while (divisor[divIdx] > num)
+                divIdx++;
+            res.append(map.get(divisor[divIdx]));
+            num -= divisor[divIdx];
         }
         return res.toString();
     }
