bf9b7d8930f7edbe40c0c8ad16b992b5a9f08203
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-24 12:16:53 +0530

    Time: 51 ms (93.59%) | Memory: 67.9 MB (39.74%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -2,44 +2,23 @@ class Solution {
     public int maximumLength(int[] nums) {
         Map<Long, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put((long) num, map.getOrDefault((long) num, 0) + 1);
+            map.put(1L * num, map.getOrDefault(1L * num, 0) + 1);
         }
-
-        int ans = 1;
-
-        if (map.containsKey(1L)) {
-            int f = map.get(1L);
-            ans = Math.max(ans, (f % 2 == 0) ? f - 1 : f);
-        }
-
-        for (long num : map.keySet()) {
-            if (num == 1L)
+        int res = 1;
+        for (Long key : map.keySet()) {
+            if (key == 1) {
+                int f = map.get(key);
+                res = Math.max(res, f % 2 == 0 ? f - 1 : f);
                 continue;
-
-            long curr = num;
-            int len = 0;
-
-            while (map.containsKey(curr)) {
-                int freq = map.get(curr);
-
-                if (freq >= 2) {
-                    len += 2;
-                } else {
-                    len += 1;
-                    break;
-                }
-
-                if (curr > 1e9 / curr)
-                    break;
-                curr = curr * curr;
             }
-
-            if (len % 2 == 0)
-                len--;
-
-            ans = Math.max(ans, len);
+            int count = 1;
+            long curr = key;
+            while (map.containsKey(curr * curr) && map.get(curr) >= 2) {
+                count += 2;
+                curr *= curr;
+            }
+            res = Math.max(res, count);
         }
-
-        return ans;
+        return res;
     }
 }
