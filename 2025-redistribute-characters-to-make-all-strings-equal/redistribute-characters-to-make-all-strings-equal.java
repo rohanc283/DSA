105d628ba62592bb76cb3ef737cbdb153d61679e
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-24 11:35:38 +0530

    Time: 3 ms (46.83%) | Memory: 46.2 MB (19.58%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean makeEqual(String[] words) {
         int n = words.length;
-        if (n == 1)
-            return true;
+        int[][] freq = new int[n][26];
 
-        int[] freq = new int[26];
-
-        for (String word : words) {
-            for (char c : word.toCharArray()) {
-                freq[c - 'a']++;
+        for (int i = 0; i < n; i++) {
+            for (char c : words[i].toCharArray()) {
+                freq[i][c - 'a']++;
             }
         }
 
-        for (int count : freq) {
-            if (count % n != 0)
+        for (int i = 0; i < 26; i++) {
+            int count = 0;
+            boolean isPresent = false;
+            for (int j = 0; j < n; j++) {
+                count += freq[j][i];
+                if (freq[j][i] > 0)
+                    isPresent = true;
+            }
+            if (isPresent && (count % n != 0))
                 return false;
         }
         return true;
