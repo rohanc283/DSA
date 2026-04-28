ac4d51128c8dbdbccddf857e847eb8c2511ad1aa
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-04-28 11:32:12 +0530

    Time: 37 ms (23.58%) | Memory: 111.3 MB (49.06%) - LeetSync

--- 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.java
+++ 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.java
@@ -1,19 +1,30 @@
 class Solution {
     public int numberOfGoodPartitions(int[] nums) {
+        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
+
+        for (int i = 0; i < n; i++) {
             map.put(nums[i], i);
         }
 
-        int i = 0, j = 0;
-        int partition = 1;
-        while (i < nums.length) {
-            if (i > j) {
-                partition = (partition * 2) % 1_000_000_007;
+        int segments = 0;
+        int maxPos = 0;
+
+        for (int i = 0; i < n; i++) {
+            maxPos = Math.max(maxPos, map.get(nums[i]));
+
+            if (maxPos == i) {
+                segments++;
             }
-            j = Math.max(j, map.get(nums[i]));
-            i++;
         }
-        return partition;
+
+        long mod = 1_000_000_007;
+        long ans = 1;
+
+        for (int i = 0; i < segments - 1; i++) {
+            ans = (ans * 2) % mod;
+        }
+
+        return (int) ans;
     }
 }
