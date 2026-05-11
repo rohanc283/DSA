d55483c294f8b503ac8c2d4c978a48abae8bd4d3
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-05-11 10:42:46 +0530

    Time: 38 ms (50.53%) | Memory: 78 MB (7.37%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.java
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.java
@@ -1,19 +1,22 @@
 class Solution {
     public int maximumSetSize(int[] nums1, int[] nums2) {
-        Set<Integer> s1 = new HashSet<>();
-        Set<Integer> s2 = new HashSet<>();
-        for (int x : nums1)
-            s1.add(x);
-        for (int x : nums2)
-            s2.add(x);
+        int n = nums1.length;
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        Set<Integer> combined = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+            combined.add(num);
+        }
 
-        int n = nums1.length / 2;
-        Set<Integer> common = new HashSet<>(s1);
-        common.retainAll(s2);
-        int take1 = Math.min(n, s1.size());
-        int take2 = Math.min(n, s2.size());
-        int c = s1.size() + s2.size() - common.size();
-        int removable = Math.max(0, take1 + take2 - c);
-        return take1 + take2 - removable;
+        for (int num : nums2) {
+            set2.add(num);
+            combined.add(num);
+        }
+
+        int taken1 = Math.min(n / 2, set1.size());
+        int taken2 = Math.min(n / 2, set2.size());
+
+        return Math.min(taken1 + taken2, taken2 + (Math.min(n / 2, combined.size() - taken2)));
     }
 }
