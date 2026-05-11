a96617a3f795343496f5f8d0ce9a2bb599eda3e5
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-05-11 10:13:29 +0530

    Time: 4 ms (16.67%) | Memory: 46.9 MB (64.29%) - LeetSync

--- 842-card-flipping-game/card-flipping-game.java
+++ 842-card-flipping-game/card-flipping-game.java
@@ -1,19 +1,28 @@
 class Solution {
     public int flipgame(int[] fronts, int[] backs) {
-        Set<Integer> same = new HashSet();
-        for (int i = 0; i < fronts.length; ++i)
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> blacklisted = new HashSet<>();
+
+        for (int i = 0; i < fronts.length; i++) {
             if (fronts[i] == backs[i])
-                same.add(fronts[i]);
+                blacklisted.add(fronts[i]);
+            else
+                set.add(backs[i]);
+        }
 
-        int ans = 9999;
-        for (int x : fronts)
-            if (!same.contains(x))
-                ans = Math.min(ans, x);
-
-        for (int x : backs)
-            if (!same.contains(x))
-                ans = Math.min(ans, x);
-
-        return ans % 9999;
+        int mini = Integer.MAX_VALUE;
+        for (int f : fronts) {
+            if (blacklisted.contains(f))
+                continue;
+            if (!set.contains(f)) {
+                mini = Math.min(mini, f);
+            }
+        }
+        for (int f : backs) {
+            if (blacklisted.contains(f))
+                continue;
+            mini = Math.min(mini, f);
+        }
+        return mini == Integer.MAX_VALUE ? 0 : mini;
     }
 }
