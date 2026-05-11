5a561b65f65464899454250e4aece44a75dd57ed
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-05-11 10:47:24 +0530

    Time: 39 ms (44.21%) | Memory: 60.6 MB (43.16%) - LeetSync

--- 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.java
+++ 3228-maximum-size-of-a-set-after-removals/maximum-size-of-a-set-after-removals.java
@@ -3,20 +3,20 @@ class Solution {
         int n = nums1.length;
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
-        Set<Integer> combined = new HashSet<>();
         for (int num : nums1) {
             set1.add(num);
-            combined.add(num);
         }
 
         for (int num : nums2) {
             set2.add(num);
-            combined.add(num);
         }
+        Set<Integer> common = new HashSet<>(set1);
+        common.retainAll(set2);
 
         int taken1 = Math.min(n / 2, set1.size());
         int taken2 = Math.min(n / 2, set2.size());
-
-        return Math.min(taken1 + taken2, taken2 + (Math.min(n / 2, combined.size() - taken2)));
+        int c = set1.size() + set2.size() - common.size();
+        int removable = Math.max(0, taken1 + taken2 - c);
+        return taken1 + taken2 - removable;
     }
 }
