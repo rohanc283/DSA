966a70806cd73371029694cf593359973847a0b5
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-05-12 10:30:24 +0530

    Time: 42 ms (26.24%) | Memory: 56.7 MB (76.90%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,42 +1,16 @@
 class Solution {
-    public int sol1(int[] nums, int k) {
-        int n = nums.length;
-        int count = 0, sum = 0;
+    public int numberOfSubarrays(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
-        for (int i = 0; i < n; i++) {
-            if (nums[i] % 2 != 0)
-                sum++;
-            int rem = sum - k;
-            if (map.containsKey(rem)) {
-                count += map.get(rem);
-            }
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-        }
-        return count;
-    }
 
-    public int numberWithAtMostK(int[] nums, int k) {
-        int n = nums.length, s = 0, e = 0;
-        int res = 0, curr = 0;
-        while (e < n) {
-            if (nums[e] % 2 != 0)
-                curr++;
-            while (curr > k) {
-                if (nums[s++] % 2 != 0)
-                    curr--;
-            }
-            res += (e - s + 1);
-            e++;
+        int oddCount = 0;
+        int res = 0;
+        for (int num : nums) {
+            oddCount += (num % 2 == 0 ? 0 : 1);
+            int target = oddCount - k;
+            res += map.getOrDefault(target, 0);
+            map.put(oddCount, map.getOrDefault(oddCount, 0) + 1);
         }
         return res;
-    }
-
-    public int sol2(int[] nums, int k) {
-        return numberWithAtMostK(nums, k) - numberWithAtMostK(nums, k - 1);
-    }
-
-    public int numberOfSubarrays(int[] nums, int k) {
-        return sol2(nums, k);
     }
 }
