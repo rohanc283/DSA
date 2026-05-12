c8b5d5c9a1baad1c43a6006b60ac578cb5db8b8f
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-05-12 17:33:05 +0530

    Time: 150 ms (30.89%) | Memory: 47.2 MB (40.00%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
@@ -1,25 +1,24 @@
 class Solution {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int rows = matrix.length, cols = matrix[0].length;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 1; j < cols; j++) {
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 matrix[i][j] += matrix[i][j - 1];
             }
         }
-        int result = 0;
-        for (int startingCol = 0; startingCol < cols; startingCol++) {
-            for (int j = startingCol; j < cols; j++) {
-                int cumSum = 0;
+        int res = 0;
+        for (int sc = 0; sc < n; sc++) {
+            for (int ec = sc; ec < n; ec++) {
                 Map<Integer, Integer> map = new HashMap<>();
-                map.put(cumSum, 1);
-                for (int r = 0; r < rows; r++) {
-                    cumSum = cumSum + matrix[r][j] - (startingCol > 0 ? matrix[r][startingCol - 1] : 0);
-                    int val = map.getOrDefault(cumSum - target, 0);
-                    result += val;
-                    map.put(cumSum, map.getOrDefault(cumSum, 0) + 1);
+                map.put(0, 1);
+                int sum = 0;
+                for (int r = 0; r < m; r++) {
+                    sum += (matrix[r][ec] - (sc > 0 ? matrix[r][sc - 1] : 0));
+                    res += map.getOrDefault(sum - target, 0);
+                    map.put(sum, map.getOrDefault(sum, 0) + 1);
                 }
             }
         }
-        return result;
+        return res;
     }
 }
