a025a87077447f3684f4aae993425b8a074f5d3f
Rohan Chaudhary <78416696+rohanc283@users.noreply.github.com>
2026-05-12 16:50:57 +0530

    Time: 293 ms (15.57%) | Memory: 54.1 MB (7.08%) - LeetSync

--- 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.java
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.java
@@ -1,19 +1,17 @@
 class Solution {
     public long wonderfulSubstrings(String word) {
-        long result = 0;
-        long xor = 0;
         Map<Long, Long> map = new HashMap<>();
         map.put(0L, 1L);
+        long xor = 0, res = 0;
         for (char c : word.toCharArray()) {
-            int shift = c - 'a';
-            xor ^= (1 << shift);
-            result += map.getOrDefault(xor, 0L);
-            for (char c1 = 'a'; c1 <= 'j'; c1++) {
-                shift = c1 - 'a';
-                result += map.getOrDefault(xor ^ (1 << shift), 0L);
+            xor ^= (1L << (c - 'a'));
+            long val = map.getOrDefault(xor, 0L);
+            res += val;
+            for (char ch = 'a'; ch <= 'j'; ch++) {
+                res += map.getOrDefault(xor ^ (1 << (ch - 'a')), 0L);
             }
-            map.put(xor, map.getOrDefault(xor, 0L) + 1L);
+            map.put(xor, val + 1);
         }
-        return result;
+        return res;
     }
 }
